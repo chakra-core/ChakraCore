4780a4ae52802c5a90cd3eb5df2617919ed233f7
Xan López <xan@igalia.com>
2016-02-01 20:03:56 +0100

    linux: make DateUtilities.cpp build
    - Define INTSAFE_E_ARITHMETIC_OVERFLOW in pal/
    - Use Int64Math to avoid overflows
    - Use the stdint.h max int64 constant

--- lib/common/common/DateUtilities.cpp
+++ lib/common/common/DateUtilities.cpp
@@ -3,13 +3,16 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 #include "CommonCommonPch.h"
+#if _WIN32
 #define ENABLE_INTSAFE_SIGNED_FUNCTIONS 1
 #include <intsafe.h>
+#endif
 
-#include "Common\DaylightTimeHelper.h"
-#include "Common\DateUtilities.h"
+#include "common/DaylightTimeHelper.h"
+#include "common/DateUtilities.h"
+#include "common/Int64Math.h"
 
-#include <Windows.Foundation.h>
+#include <stdint.h>
 
 namespace Js
 {
@@ -113,13 +116,12 @@ namespace Js
 
         if (!NumberUtilities::IsValidTryToInt64(es5DateAsInt64)) return INTSAFE_E_ARITHMETIC_OVERFLOW;
 
-        INT64 numTicks;
-        // We use the LongLong* functions since that's typedef'd to int64
         // First, we rebase it to the WinRT epoch, then we convert the time in milliseconds to ticks
-        if (SUCCEEDED(::Int64Add(es5DateAsInt64, jsEpochMilliseconds, reinterpret_cast<LONGLONG*>(&numTicks))))
+        INT64 numTicks;
+        if (!Int64Math::Add(es5DateAsInt64, jsEpochMilliseconds, &numTicks))
         {
             INT64 adjustedTicks = 0;
-            if (SUCCEEDED(::Int64Mult(numTicks, ticksPerMillisecond, reinterpret_cast<LONGLONG*>(&adjustedTicks))))
+            if (!Int64Math::Mul(numTicks, ticksPerMillisecond, &adjustedTicks))
             {
                 (*pRet) = adjustedTicks;
                 return S_OK;
@@ -165,7 +167,7 @@ namespace Js
         }
 
         //Otherwise the double multiplication might overflow
-        if (span > MAXINT64 / ticksPerMillisecond)
+        if (span > INT64_MAX / ticksPerMillisecond)
         {
             return INTSAFE_E_ARITHMETIC_OVERFLOW;
         }
