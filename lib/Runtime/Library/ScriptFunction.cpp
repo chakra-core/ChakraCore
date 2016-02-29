0f68dd295fdba066326b080c362b7fbe56093c8c
Lei Shi <leish@microsoft.com>
2016-02-29 15:05:43 -0800

    remove the unecessary change

--- lib/Runtime/Library/ScriptFunction.cpp
+++ lib/Runtime/Library/ScriptFunction.cpp
@@ -594,7 +594,7 @@ namespace Js
                     {
                         memset(this->m_inlineCaches[i], 0, sizeof(InlineCache));
                     }
-                    else if(scriptContext != nullptr && !scriptContext->IsClosed())
+                    else if(!scriptContext->IsClosed())
                     {
                         if (inlineCache->RemoveFromInvalidationList())
                         {
