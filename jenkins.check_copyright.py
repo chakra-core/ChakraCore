17a1ac059370a7d5d017c12e26587c8def35372d
Doug Ilijev <doilij@microsoft.com>
2016-02-29 14:29:12 -0800

    Copyright check: use python 3.x print function to make the script independent of the version of python installed.

--- jenkins.check_copyright.py
+++ jenkins.check_copyright.py
@@ -3,7 +3,8 @@
 # Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 #-------------------------------------------------------------------------------------------------------
 
-# Python 2.7
+# Python 2.7 and 3.x compatibility for easier testing regardless of python installation
+from __future__ import print_function
 
 import sys
 import os.path
@@ -21,12 +22,12 @@ for line in copyright_lines:
     regexes.append(re.compile(pattern))
 
 if len(sys.argv) < 2:
-    print "Requires passing a filename as an argument."
+    print("Requires passing a filename as an argument.")
     exit(1)
 
 file_name = sys.argv[1]
 if not os.path.isfile(file_name):
-    print "File does not exist:", file_name, "(not necessarily an error)"
+    print("File does not exist:", file_name, "(not necessarily an error)")
     exit(0)
 
 with open(file_name, 'r') as sourcefile:
@@ -36,6 +37,6 @@ with open(file_name, 'r') as sourcefile:
         line = line.rstrip()
         matches = regexes[x].match(line)
         if not matches:
-            print file_name, "... does not contain a correct Microsoft copyright notice."
+            print(file_name, "... does not contain a correct Microsoft copyright notice.")
             # found a problem so exit and report the problem to the caller
             exit(1)
