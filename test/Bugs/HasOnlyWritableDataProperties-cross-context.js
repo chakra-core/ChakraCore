d50b90f0fd055b99baf1acdd4de76cd14c16b8ad
Gorkem Yakin <goyakin@microsoft.com>
2016-04-14 10:43:30 -0700

    Clear the HasOnlyWritableDataProperties cache across script contexts when prototype changes

---
+++ test/Bugs/HasOnlyWritableDataProperties-cross-context.js
@@ -0,0 +1,13 @@
+function f() {
+    var obj0 = {};
+    obj1 = Object.create(obj0);
+
+    var sc = WScript.LoadScript('function setUp(obj0, obj1) { obj1.foo0 = 1; Object.setPrototypeOf(obj0, {}); Object.defineProperty(Object.getPrototypeOf(obj0), "foo1", {writable: false, value: "bar"}); }', 'samethread');
+    sc.setUp(obj0, obj1);
+
+    obj1.foo2 = 'bar';
+}
+
+f();
+
+WScript.Echo('Pass');
