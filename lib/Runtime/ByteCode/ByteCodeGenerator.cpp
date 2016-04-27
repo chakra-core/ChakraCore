3fff752f1193ed55f40bb8fc2e26431887724b7c
Aneesh Divakarakurup <aneeshd@microsoft.com>
2016-04-27 14:25:26 -0700

    Fixing scope issues with function expressions with split scope
    This change fixes multiple issues with using function expression symbols in split scope.
    
    One issue was if the function expression is not captured and has not eval
    then we were adding the function expression symbol to body scope. In split
    scope case I changed it to add to the param scope. But we don't have to
    duplicate the function expression name symbol in the body.
    
    Function expression should be pop out only after taking out the param scope. Another issue was while creating the new frame display during BeginBodyScope we have to skip the function expression.
    
    In split scope the body scope was always getting marked as must
    instantiate even though no closure was there in the body scope. I added a
    new flag to the scope object to mark whether a closure is there.

--- lib/Runtime/ByteCode/ByteCodeGenerator.cpp
+++ lib/Runtime/ByteCode/ByteCodeGenerator.cpp
@@ -998,14 +998,9 @@ void ByteCodeGenerator::RestoreScopeInfo(Js::FunctionBody* functionBody)
                 bodyScope = Anew(alloc, Scope, alloc, ScopeType_FunctionBody, true);
             }
         }
+        bodyScope->SetHasOwnLocalInClosure(scopeInfo->GetHasOwnLocalInClosure());
 
         FuncInfo* func = Anew(alloc, FuncInfo, functionBody->GetDisplayName(), alloc, paramScope, bodyScope, nullptr, functionBody);
-
-        if (paramScope != nullptr)
-        {
-            paramScope->SetFunc(func);
-            paramScopeInfo->GetScopeInfo(nullptr, this, func, paramScope);
-        }
 
         if (bodyScope->GetScopeType() == ScopeType_GlobalEvalBlock)
         {
@@ -1032,6 +1027,12 @@ void ByteCodeGenerator::RestoreScopeInfo(Js::FunctionBody* functionBody)
             funcExprScopeInfo->GetScopeInfo(nullptr, this, func, funcExprScope);
         }
 
+        // Restore the param scope after the function expression scope
+        if (paramScope != nullptr)
+        {
+            paramScope->SetFunc(func);
+            paramScopeInfo->GetScopeInfo(nullptr, this, func, paramScope);
+        }
         scopeInfo->GetScopeInfo(nullptr, this, func, bodyScope);
     }
     else
