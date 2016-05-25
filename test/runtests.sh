17faae9cc9c05366645c93b05d9bb45a513076e4
Oguz Bastemur <ogbastem@microsoft.com>
2016-05-25 11:05:29 -0700

    cross platform: fix runtest.sh start path and force build flavor

--- test/runtests.sh
+++ test/runtests.sh
@@ -3,17 +3,27 @@
 # Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 #-------------------------------------------------------------------------------------------------------
 #
-# todo-CI: REMOVE THIS AFTER ENABLING runtests.py on CI
+# todo-CI: REMOVE THIS AFTER ENABLING runtests.py directly on CI
 
 test_path=`dirname "$0"`
 
-# Only debug jenkins builds support tests currently
-# Todo: Support both debug and test builds
-ch_path="$test_path/../BuildLinux/debug/ch"
-
-if [ ! -f $ch_path ]; then
-    echo 'ch not found- exiting'
-    exit 1
+build_type=$1
+# Accept -d or -t. If none was given (i.e. current CI), 
+# search for the known paths
+if [[ $build_type != "-d" && $build_type != "-t" ]]; then
+    echo "Warning: You haven't provide either '-d' (debug) or '-t' (test)."
+    echo "Warning: Searching for ch.."
+    if [[ -f "$test_path/../BuildLinux/debug/ch" ]]; then
+        echo "Warning: Debug build was found"
+        build_type="-d"
+    elif [[ -f "$test_path/../BuildLinux/test/ch" ]]; then
+        echo "Warning: Test build was found"
+        build_type="-t"
+    else
+        echo 'Error: ch not found- exiting'
+        exit 1
+    fi
 fi
 
-"$test_path/runtests.py" --binary $ch_path Basics/hello.js
+# todo-xpat: Enable others tests 
+"$test_path/runtests.py" $build_type $test_path/Basics/hello.js
