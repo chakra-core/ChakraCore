a78cfb63f44e700d97815a8b1c2cbb0dd1eee622
marron <marron@microsoft.com>
2016-06-16 12:08:00 -0700

    Quick fix.

--- bin/ch/Helpers.cpp
+++ bin/ch/Helpers.cpp
@@ -310,7 +310,7 @@ void Helpers::TTReportLastIOErrorAsNeeded(BOOL ok, char* msg)
         LPTSTR pTemp = NULL;
         FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ARGUMENT_ARRAY, NULL, lastError, 0, (LPTSTR)&pTemp, 0, NULL);
         fwprintf(stderr, _u(": %s"), pTemp);
-        fwprintf(stderr, _u("msg is: %s"), msg);
+        fprintf(stderr, "msg is: %s", msg);
 
         AssertMsg(false, "IO Error!!!");
     }
