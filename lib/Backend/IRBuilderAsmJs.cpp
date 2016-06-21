927f5079a51d6fb2e58756871b59689b8fae5087
George Kuan <commonlisp@gmail.com>
2016-06-21 09:17:05 -0700

    interpreter semantics is different from CRT so cannot reuse there

--- lib/Backend/IRBuilderAsmJs.cpp
+++ lib/Backend/IRBuilderAsmJs.cpp
@@ -2919,6 +2919,7 @@ IRBuilderAsmJs::BuildFloat3(Js::OpCodeAsmJs newOpcode, uint32 offset, Js::RegSlo
 
     case Js::OpCodeAsmJs::Copysign_Flt:
         instr = IR::Instr::New(Js::OpCode::Copysign_A, dstOpnd, src1Opnd, src2Opnd, m_func);
+        break;
 
     case Js::OpCodeAsmJs::Min_Flt:
         instr = IR::Instr::New(Js::OpCode::InlineMathMin, dstOpnd, src1Opnd, src2Opnd, m_func);
