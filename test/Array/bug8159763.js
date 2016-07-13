82ceb21282ce05ccd034ec578e4e1f4f8e7b651c
Michael Ferris <micfer@Microsoft.com>
2016-07-13 10:15:17 -0700

    Bailout on Memset/Memcopy if the length is <= 0. After the memop, we restore the induction variable to its expected value. If the memop didn't actually happen we shouldn't modify the induction variable.
    This is unlikely to happen since we would normally bailout on no profile and not emit this memop.

---
+++ test/Array/bug8159763.js
@@ -0,0 +1,15 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
+function test0() {
+  var IntArr0 = [];
+  v5 = IntArr0.length;
+  for (var i = 10; i < v5; i++) {
+    IntArr0[i] = 0.5;
+  }
+  WScript.Echo(i);
+}
+test0();
+test0();
