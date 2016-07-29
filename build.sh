027df2508d56a0790dda76bfa46ed2dbb47c699d
Torben Köhn <t.koehn@outlook.com>
2016-07-29 12:54:16 +0200

    Added shebang to build script
    This avoids build problems when you're using another shell like zsh.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 #-------------------------------------------------------------------------------------------------------
 # Copyright (C) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
