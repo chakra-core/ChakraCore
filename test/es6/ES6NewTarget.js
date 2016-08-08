fc08efb0382b9a1aa1f3f3828074262cbf36b24b
Suwei Chen <suwc@microsoft.com>
2016-08-07 22:35:47 -0700

    Special cases of new.target
    Fix some special cases of 'new.target' reference, e.g.:
    
    - 'new.target' inside direct eval() in global function throws SyntaxError
    - 'new.target' inside indirect eval() throws SyntaxError
    - 'new.target' inside function evaluates to undefined for function call
    - 'new.target' inside function evaluates to said function for constructor call
    - lambda+eval in global function called in a function (SyntaxError)
    - lambda+eval in a function called by global function (okay)
    - other nested/compounded cases with eval(), arrow functions, and functions

--- test/es6/ES6NewTarget.js
+++ test/es6/ES6NewTarget.js
@@ -49,8 +49,6 @@ var tests = [
             assert.areEqual('something', obj.target, "The name 'target' can be used as an identifier");
         }
     },
-/*
-    // Blocked by 'ReferenceError: '<_lexicalNewTargetSymbol>' is undefined' bug
     {
         name: "new.target is not valid for assignment",
         body: function() {
@@ -58,7 +56,7 @@ var tests = [
             assert.throws(function() { eval("((new.target)) = 'something';"); }, ReferenceError, "new.target cannot be a lhs in an assignment expression - this is an early reference error", "Invalid left-hand side in assignment");
         }
     },
-*/
+
     {
         name: "Simple base class gets new.target correctly",
         body: function() {
@@ -273,8 +271,83 @@ var tests = [
             assert.isTrue((foo()).next().value == undefined, "Generator function has new.target set to undefined in the function body");
         }
     },
+    {
+        name: "new.target inside eval() in function",
+        body: function() {
+            function func() {
+               var g = ()=>eval('new.target;');
+               return g();
+            }
 
+            assert.areEqual(undefined, func(), "plain function call");
+            assert.areEqual(undefined, eval("func()"), "function call inside eval");
+            assert.areEqual(undefined, eval("eval('func()')"), "function call inside nested evals");
+            assert.areEqual(undefined, (()=>func())(), "function call inside arrow function");
+            assert.areEqual(undefined, (()=>(()=>func())())(), "function call inside nested arrow functions");
+            assert.areEqual(undefined, eval("(()=>func())()"), "function call inside arrow function inside eval");
+            assert.areEqual(undefined, (()=>eval("func()"))(), "function call inside eval inside arrow function");
+            assert.areEqual(undefined, eval("(()=>eval('func()'))()"), "function call inside eval inside arrow function inside eval");
 
+            assert.areEqual(func, new func(), "plain constructor call");
+            assert.areEqual(func, eval("new func()"), "constructor call inside eval");
+            assert.areEqual(func, eval("eval('new func()')"), "constructor call inside nested evals");
+            assert.areEqual(func, (()=>new func())(), "constructor call inside arrow function");
+            assert.areEqual(func, (()=>(()=>new func())())(), "constructor call inside nested arrow functions");
+            assert.areEqual(func, eval("(()=>new func())()"), "constructor call inside arrow function inside eval");
+            assert.areEqual(func, (()=>eval("new func()"))(), "constructor call inside eval inside arrow function");
+            assert.areEqual(func, eval("(()=>eval('new func()'))()"), "constructor call inside eval inside arrow function inside eval");
+        }
+    },
+    {
+        name: "new.target inside netsted eval, arrow function, and function defintion through eval",
+        body: function() {
+            eval("function func() {var f = ()=>{function g() {}; return eval('new.target')}; return f(); }" );
+
+            assert.areEqual(undefined, func(), "plain function call");
+            assert.areEqual(undefined, eval("func()"), "function call inside eval");
+            assert.areEqual(undefined, eval("eval('func()')"), "function call inside nested evals");
+            assert.areEqual(undefined, (()=>func())(), "function call inside arrow function");
+            assert.areEqual(undefined, (()=>(()=>func())())(), "function call inside nested arrow functions");
+            assert.areEqual(undefined, eval("(()=>func())()"), "function call inside arrow function inside eval");
+            assert.areEqual(undefined, (()=>eval("func()"))(), "function call inside eval inside arrow function");
+            assert.areEqual(undefined, eval("(()=>eval('func()'))()"), "function call inside eval inside arrow function inside eval");
+
+            assert.areEqual(func, new func(), "plain constructor call");
+            assert.areEqual(func, eval("new func()"), "constructor call inside eval");
+            assert.areEqual(func, eval("eval('new func()')"), "constructor call inside nested evals");
+            assert.areEqual(func, (()=>new func())(), "constructor call inside arrow function");
+            assert.areEqual(func, (()=>(()=>new func())())(), "constructor call inside nested arrow functions");
+            assert.areEqual(func, eval("(()=>new func())()"), "constructor call inside arrow function inside eval");
+            assert.areEqual(func, (()=>eval("new func()"))(), "constructor call inside eval inside arrow function");
+            assert.areEqual(func, eval("(()=>eval('new func()'))()"), "constructor call inside eval inside arrow function inside eval");
+        }
+    },
+    {
+        name: "direct and indirect eval with new.target",
+        body: function() {
+            function scriptThrows(func, errType, info, errMsg) {
+                try {
+                    func();
+                    throw Error("No exception thrown");
+                } catch (err) {
+                    assert.areEqual(errType.name + ':' + errMsg, err.name + ':' + err.message, info);
+               }
+            }
+
+            scriptThrows(()=>{ WScript.LoadScript("eval('new.target')", "samethread"); }, SyntaxError, "direct eval in global function", "Invalid use of the 'new.target' keyword");
+            scriptThrows(()=>{ WScript.LoadScript("(()=>eval('new.target'))();", "samethread"); }, SyntaxError, "direct eval in lambda in global function", "Invalid use of the 'new.target' keyword");
+            scriptThrows(()=>{ WScript.LoadScript("var f=()=>eval('new.target'); (function() { return f(); })();", "samethread"); }, SyntaxError, "direct eval in lambda in global function called by a function", "Invalid use of the 'new.target' keyword");
+            assert.doesNotThrow(()=>{ WScript.LoadScript("(function() { eval('new.target;') })()", "samethread"); }, "direct eval in function");
+            assert.doesNotThrow(()=>{ WScript.LoadScript("var f =(function() { return ()=>eval('new.target;') })(); f();", "samethread"); }, "direct eval in lambda defined in function and called by global function");
+
+            scriptThrows(()=>{ WScript.LoadScript("(0, eval)('new.target;')", "samethread"); }, SyntaxError, "indirect eval in global function", "Invalid use of the 'new.target' keyword");
+            scriptThrows(()=>{ WScript.LoadScript("(()=>(0, eval)('new.target'))();", "samethread"); }, SyntaxError, "indirect eval in lambda in global function", "Invalid use of the 'new.target' keyword");
+            scriptThrows(()=>{ WScript.LoadScript("var f=()=>(0, eval)('new.target'); (function() { return f(); })();", "samethread"); }, SyntaxError, "indirect eval in lambda in global function called by a function", "Invalid use of the 'new.target' keyword");
+            scriptThrows(()=>{ WScript.LoadScript("(function() { (0, eval)('new.target;') })()", "samethread")}, SyntaxError, "indirect eval in function", "Invalid use of the 'new.target' keyword");
+            scriptThrows(()=>{ WScript.LoadScript("var f =(function() { return ()=>(0, eval)('new.target;') })(); f();", "samethread"); }, SyntaxError, "indirect eval in lambda defined in function and called by global function", "Invalid use of the 'new.target' keyword");
+
+        }
+    },
 ];
 
 testRunner.runTests(tests, { verbose: WScript.Arguments[0] != "summary" });
