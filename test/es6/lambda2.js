678be27bbc01d7e7850dae8577bda030b5ad35b8
Tom Tan <totan@microsoft.com>
2016-08-29 23:31:40 -0700

    Fix incorrect identifier recording of lambda parameter

---
+++ test/es6/lambda2.js
@@ -0,0 +1,14 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
+{
+  function foo() {
+  }
+  function bar() {
+      foo => 1
+  }
+}
+
+WScript.Echo("PASS");
