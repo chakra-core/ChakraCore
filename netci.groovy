17f48e7585956eb31f1b88446a9c72ca335f5fff
Doug Ilijev <doilij@microsoft.com>
2016-09-20 13:14:17 -0700

    Test tab check success.

--- netci.groovy
+++ netci.groovy
@@ -3,6 +3,10 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 
+if (true) {
+    print("hello")
+}
+
 // Import the utility functionality.
 import jobs.generation.Utilities
 
