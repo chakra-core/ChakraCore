1df6bdc6ebf526f2b13f39c926c39a5b7eaf00dc
Tom Tan <totan@microsoft.com>
2016-09-27 17:48:38 -0700

    Set src1 on BailOnNotStackArgs to do CSE correctly

--- lib/Backend/Inline.cpp
+++ lib/Backend/Inline.cpp
@@ -2306,6 +2306,8 @@ IR::Instr * Inline::InlineApplyWithArgumentsObject(IR::Instr * callInstr, IR::In
         bailOutOnNotStackArgsInsertionPoint = primaryBailoutInstr;
     }
 
+    // set src1 to avoid CSE on BailOnNotStackArgs for different arguments object
+    bailOutOnNotStackArgs->SetSrc1(ldHeapArguments->GetDst());
     bailOutOnNotStackArgsInsertionPoint->InsertBefore(bailOutOnNotStackArgs);
 
     // If we optimized the call instruction for a fixed function, we must extend the function object's lifetime until after
@@ -2549,6 +2551,8 @@ bool Inline::InlineApplyTarget(IR::Instr *callInstr, const FunctionJITTimeInfo* 
 
     IR::Instr *  bailOutOnNotStackArgs = IR::BailOutInstr::New(Js::OpCode::BailOnNotStackArgs, IR::BailOutOnInlineFunction,
         callInstr, callInstr->m_func);
+    // set src1 to avoid CSE on BailOnNotStackArgs for different arguments object
+    bailOutOnNotStackArgs->SetSrc1(argumentsObjArgOut->GetSrc1());
     argumentsObjArgOut->InsertBefore(bailOutOnNotStackArgs);
 
     IR::Instr* byteCodeArgOutUse = IR::Instr::New(Js::OpCode::BytecodeArgOutUse, callInstr->m_func);
