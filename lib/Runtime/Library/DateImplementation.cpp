416e54c49f43b45ab289afe3faa3d801e53f8d18
Doug Ilijev <doilij@microsoft.com>
2016-09-30 16:04:05 -0700

    Update Date.UTC to not return NaN for 1 argument. Fixes #1665.
    Fixes #1665: Date.UTC(x) [one-argument] spec change.

--- lib/Runtime/Library/DateImplementation.cpp
+++ lib/Runtime/Library/DateImplementation.cpp
@@ -1582,9 +1582,9 @@ LError:
         uint ivar;
 
         // See: https://github.com/Microsoft/ChakraCore/issues/1318
-        // Date.UTC should return NaN with < 2 arguments.
-        // args.Info.Count includes an implicit first parameter, so we check for Count <= 2.
-        if (args.Info.Count <= 2)
+        // Date.UTC should return NaN with 0 arguments.
+        // args.Info.Count includes an implicit first parameter, so we check for Count <= 1.
+        if (args.Info.Count <= 1)
         {
             return JavascriptNumber::NaN;
         }
