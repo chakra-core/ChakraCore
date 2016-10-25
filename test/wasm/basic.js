579b67bcdf57ad1459e2a363d1b4ae5f5018f851
Michael Holman <Michael.Holman@microsoft.com>
2016-10-24 21:51:59 -0700

    add test using WebAssembly.compile

--- test/wasm/basic.js
+++ test/wasm/basic.js
@@ -3,13 +3,19 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 
-var a = Wasm.instantiateModule(readbuffer('basic.wasm'), {test: {foo: function(a){print(a); return 2;}}});
-print(a.exports.a(11));
-print(a.exports.a(11));
-var b = 0;
-var c = new Int32Array(a.exports.memory);
-for(var i=0; i<10000; i++)
+var module = WebAssembly.compile(readbuffer('basic.wasm'));
+module.then(function(val)
 {
-    b+= c[i];
-}
-print(b);
+    var a = new WebAssembly.Instance(val, {test: {foo: function(a){print(a); return 2;}}});
+    print(a.exports.a(11));
+    print(a.exports.a(11));
+    var b = 0;
+    var c = new Int32Array(a.exports.memory);
+    for(var i=0; i<10000; i++)
+    {
+        b+= c[i];
+    }
+    print(b);
+
+})
+.catch(function(reason){print(reason)});
