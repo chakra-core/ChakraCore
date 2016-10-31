dd466e086313e14491a2e4107e85463df7ee76e5
Jeff Weiner <jdweiner@microsoft.com>
2016-10-31 14:51:55 -0700

    Module hangs if it imports an errored module
    ResolveExternalModuleDependencies has no problem today resolving against a
    module that has already been determined to be in an error state.  In this
    case, it has already notified its dependents at the time of its failure,
    and no further notification will occur.  As a result, the module hangs if
    we do not handle this scenario.
    
    This fix addresses the issue by not proceeding if the module being
    imported is already in an error state.  The error is passed upward.

--- lib/Runtime/Language/SourceTextModuleRecord.cpp
+++ lib/Runtime/Language/SourceTextModuleRecord.cpp
@@ -559,6 +559,7 @@ namespace Js
                 ArenaAllocator* allocator = scriptContext->GeneralAllocator();
                 childrenModuleSet = (ChildModuleRecordSet*)AllocatorNew(ArenaAllocator, allocator, ChildModuleRecordSet, allocator);
             }
+            Var error = nullptr;
             requestedModuleList->MapUntil([&](IdentPtr specifier) {
                 ModuleRecordBase* moduleRecordBase = nullptr;
                 SourceTextModuleRecord* moduleRecord = nullptr;
@@ -572,6 +573,12 @@ namespace Js
                         return true;
                     }
                     moduleRecord = SourceTextModuleRecord::FromHost(moduleRecordBase);
+                    if (moduleRecord->errorObject != nullptr)
+                    {
+                        hr = E_FAIL;
+                        error = moduleRecord->errorObject;
+                        return true;
+                    }
                     childrenModuleSet->AddNew(moduleName, moduleRecord);
                     if (moduleRecord->parentModuleList == nullptr)
                     {
@@ -587,8 +594,12 @@ namespace Js
             });
             if (FAILED(hr))
             {
-                JavascriptError *error = scriptContext->GetLibrary()->CreateError();
-                JavascriptError::SetErrorMessageProperties(error, hr, _u("fetch import module failed"), scriptContext);
+                if (error == nullptr)
+                {
+                    JavascriptError *jsError = scriptContext->GetLibrary()->CreateError();
+                    JavascriptError::SetErrorMessageProperties(jsError, hr, _u("fetch import module failed"), scriptContext);
+                    error = jsError;
+                }
                 this->errorObject = error;
                 NotifyParentsAsNeeded();
             }
