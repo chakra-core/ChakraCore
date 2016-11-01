36f9693947b59278085b28ca3f2330339df16d74
Nikolay Korovaiko <korovaikon@gmail.com>
2016-11-01 10:43:40 -0700

    optimizer part

--- test/wasm/exceptions.js
+++ test/wasm/exceptions.js
@@ -13,6 +13,9 @@ const blob = WScript.LoadBinaryFile('exceptions.wasm');
 const moduleBytesView = new Uint8Array(blob);
 var a = Wasm.instantiateModule(moduleBytesView, {}).exports;
 
+print(trap(a.i32_div_s_by_two, 5, 2));
+print(trap(a.i32_div_s_over, 5, 2));
+print(trap(a.i32_div_s_over, 5, 0));
 print(trap(a.i32_div_s, 5, 0));
 print(trap(a.i32_div_s, -2147483648, -1));
 print(trap(a.i32_div_u, 5, 0));
