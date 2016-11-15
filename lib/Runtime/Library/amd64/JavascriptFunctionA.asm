4e0b158936adf8725cc7344055a2ddc5986e660b
Michael Holman <Michael.Holman@microsoft.com>
2016-11-14 17:39:47 -0800

    fix #ifdef in amd64 asm thunks to use correct define

--- lib/Runtime/Library/amd64/JavascriptFunctionA.asm
+++ lib/Runtime/Library/amd64/JavascriptFunctionA.asm
@@ -220,7 +220,7 @@ done:
 
 amd64_CallFunction ENDP
 
-ifdef _ENABLE_ASM_JS
+ifdef _ENABLE_DYNAMIC_THUNKS
 
 extrn ?GetStackSizeForAsmJsUnboxing@Js@@YAHPEAVScriptFunction@1@@Z: PROC
 extrn ?GetArgsSizesArray@Js@@YAPEAIPEAVScriptFunction@1@@Z : PROC
@@ -418,7 +418,7 @@ done:
 
 ??$CallAsmJsFunction@H@JavascriptFunction@Js@@SAHPEAVRecyclableObject@1@P6APEAX0UCallInfo@1@ZZIPEAPEAX@Z ENDP
 
-endif ;; _ENABLE_ASM_JS
+endif ;; _ENABLE_DYNAMIC_THUNKS
 
 extrn ?DeferredParse@JavascriptFunction@Js@@SAP6APEAXPEAVRecyclableObject@2@UCallInfo@2@ZZPEAPEAVScriptFunction@2@@Z : PROC
 align 16
