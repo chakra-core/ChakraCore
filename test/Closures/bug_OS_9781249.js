98feda1ae27e86e3b994096b99eec5d567269f98
Rajat Dua <radua@microsoft.com>
2016-11-15 18:12:20 -0800

    copyright check

--- test/Closures/bug_OS_9781249.js
+++ test/Closures/bug_OS_9781249.js
@@ -1,3 +1,8 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
 function test0() {
   var obj0 = {};
   var arrObj0 = {};
