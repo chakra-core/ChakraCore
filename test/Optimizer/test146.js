01c06832227646f32096b3a15b304c41853150c0
Rajat Dua <radua@microsoft.com>
2016-12-15 11:31:10 -0800

    Adding unit test

---
+++ test/Optimizer/test146.js
@@ -0,0 +1,32 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
+var obj1 = {};
+var litObj0 = {};
+var func1 = function () {
+};
+var func2 = function () {
+  for (var _strvar1 in ui16) {
+    if (_strvar1.indexOf() != -1) {
+      continue;
+    }
+    '$'.indexOf();
+    var z = --this.prop0 * Boolean;
+  }
+  WScript.Echo();
+};
+var func4 = function () {
+  func2.call(litObj0), ({ prop0: Object({ prop1: func2() }) });
+};
+obj1.method0 = func1;
+var ui16 = new Uint16Array(256);
+
+prop0 = -87;
+
+func4();
+func4();
+func4();
+  
+WScript.Echo(prop0);
