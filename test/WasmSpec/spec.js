bcc1db2f827ec849e50c2f1a18ca6b517b0c142f
Michael Ferris <micfer@microsoft.com>
2016-12-20 14:07:21 -0800

    Code review fix

--- test/WasmSpec/spec.js
+++ test/WasmSpec/spec.js
@@ -2,6 +2,23 @@
 // Copyright (C) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
+// This file has been modified by Microsoft on [12/2016].
+
+/*
+ * Copyright 2016 WebAssembly Community Group participants
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 const cliArgs = WScript.Arguments || [];
 WScript.Flag("-wasmI64");
