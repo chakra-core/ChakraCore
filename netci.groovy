22676601ff577e416286ec6d88f2fe46cc8bdcbb
Doug Ilijev <doilij@microsoft.com>
2017-01-05 15:29:15 -0800

    Refactor CreateBuildTasks to call CreateBuildTask to allow re-use of that logic.

--- netci.groovy
+++ netci.groovy
@@ -39,76 +39,80 @@ if (branch.startsWith('master')) {
 // HELPER CLOSURES
 // ---------------
 
+def CreateBuildTask = { isPR, buildArch, buildType, machine, configTag, buildExtra, testExtra, runCodeAnalysis, excludeConfigIf, nonDefaultTaskSetup ->
+    if (excludeConfigIf && excludeConfigIf(isPR, buildArch, buildType)) {
+        return // early exit: we don't want to create a job for this configuration
+    }
+
+    def config = "${buildArch}_${buildType}"
+    config = (configTag == null) ? config : "${configTag}_${config}"
+
+    // params: Project, BaseTaskName, IsPullRequest (appends '_prtest')
+    def jobName = Utilities.getFullJobName(project, config, isPR)
+
+    def testableConfig = buildType in ['debug', 'test'] && buildArch != 'arm'
+    def analysisConfig = buildType in ['release'] && runCodeAnalysis
+
+    def buildScript = "call .\\jenkins\\buildone.cmd ${buildArch} ${buildType} "
+    buildScript += buildExtra ?: ''
+    buildScript += analysisConfig ? ' "/p:runcodeanalysis=true"' : ''
+    def testScript = "call .\\jenkins\\testone.cmd ${buildArch} ${buildType} "
+    testScript += testExtra ?: ''
+    def analysisScript = '.\\Build\\scripts\\check_prefast_error.ps1 . CodeAnalysis.err'
+
+    def newJob = job(jobName) {
+        // This opens the set of build steps that will be run.
+        // This looks strange, but it is actually a method call, with a
+        // closure as a param, since Groovy allows method calls without parens.
+        // (Compare with '.each' method used above.)
+        steps {
+            batchFile(buildScript) // run the parameter as if it were a batch file
+            if (testableConfig) {
+                batchFile(testScript)
+            }
+            if (analysisConfig) {
+                powerShell(analysisScript)
+            }
+        }
+    }
+
+    def msbuildType = msbuildTypeMap.get(buildType)
+    def msbuildFlavor = "build_${buildArch}${msbuildType}"
+    def archivalString = "test/${msbuildFlavor}.*,test/logs/**"
+    archivalString += analysisConfig ? ',CodeAnalysis.err' : ''
+    Utilities.addArchival(newJob, archivalString,
+        '', // no exclusions from archival
+        false, // doNotFailIfNothingArchived=false ~= failIfNothingArchived
+        false) // archiveOnlyIfSuccessful=false ~= archiveAlways
+
+    Utilities.setMachineAffinity(newJob, machine, 'latest-or-auto')
+    Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
+
+    if (nonDefaultTaskSetup == null) {
+        if (isPR) {
+            def osTag = machineTypeToOSTagMap.get(machine)
+            // Set up checks which apply to PRs targeting any branch
+            Utilities.addGithubPRTrigger(newJob, "${osTag} ${config}")
+            // To enable PR checks only for specific target branches, use the following instead:
+            // Utilities.addGithubPRTriggerForBranch(newJob, branch, checkName)
+        } else {
+            Utilities.addGithubPushTrigger(newJob)
+        }
+    } else {
+        // nonDefaultTaskSetup is e.g. DailyBuildTaskSetup (which sets up daily builds)
+        // These jobs will only be configured for the branch specified below,
+        // which is the name of the branch netci.groovy was processed for.
+        // See list of such branches at:
+        // https://github.com/dotnet/dotnet-ci/blob/master/jobs/data/repolist.txt
+        nonDefaultTaskSetup(newJob, isPR, config)
+    }
+}
+
 def CreateBuildTasks = { machine, configTag, buildExtra, testExtra, runCodeAnalysis, excludeConfigIf, nonDefaultTaskSetup ->
     jobTypesToGenerate.each { isPR ->
         ['x86', 'x64', 'arm'].each { buildArch ->
             ['debug', 'test', 'release'].each { buildType ->
-                if (excludeConfigIf && excludeConfigIf(isPR, buildArch, buildType)) {
-                    return // early exit: we don't want to create a job for this configuration
-                }
-
-                def config = "${buildArch}_${buildType}"
-                config = (configTag == null) ? config : "${configTag}_${config}"
-
-                // params: Project, BaseTaskName, IsPullRequest (appends '_prtest')
-                def jobName = Utilities.getFullJobName(project, config, isPR)
-
-                def testableConfig = buildType in ['debug', 'test'] && buildArch != 'arm'
-                def analysisConfig = buildType in ['release'] && runCodeAnalysis
-
-                def buildScript = "call .\\jenkins\\buildone.cmd ${buildArch} ${buildType} "
-                buildScript += buildExtra ?: ''
-                buildScript += analysisConfig ? ' "/p:runcodeanalysis=true"' : ''
-                def testScript = "call .\\jenkins\\testone.cmd ${buildArch} ${buildType} "
-                testScript += testExtra ?: ''
-                def analysisScript = '.\\Build\\scripts\\check_prefast_error.ps1 . CodeAnalysis.err'
-
-                def newJob = job(jobName) {
-                    // This opens the set of build steps that will be run.
-                    // This looks strange, but it is actually a method call, with a
-                    // closure as a param, since Groovy allows method calls without parens.
-                    // (Compare with '.each' method used above.)
-                    steps {
-                        batchFile(buildScript) // run the parameter as if it were a batch file
-                        if (testableConfig) {
-                            batchFile(testScript)
-                        }
-                        if (analysisConfig) {
-                            powerShell(analysisScript)
-                        }
-                    }
-                }
-
-                def msbuildType = msbuildTypeMap.get(buildType)
-                def msbuildFlavor = "build_${buildArch}${msbuildType}"
-                def archivalString = "test/${msbuildFlavor}.*,test/logs/**"
-                archivalString += analysisConfig ? ',CodeAnalysis.err' : ''
-                Utilities.addArchival(newJob, archivalString,
-                    '', // no exclusions from archival
-                    false, // doNotFailIfNothingArchived=false ~= failIfNothingArchived
-                    false) // archiveOnlyIfSuccessful=false ~= archiveAlways
-
-                Utilities.setMachineAffinity(newJob, machine, 'latest-or-auto')
-                Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
-
-                if (nonDefaultTaskSetup == null) {
-                    if (isPR) {
-                        def osTag = machineTypeToOSTagMap.get(machine)
-                        // Set up checks which apply to PRs targeting any branch
-                        Utilities.addGithubPRTrigger(newJob, "${osTag} ${config}")
-                        // To enable PR checks only for specific target branches, use the following instead:
-                        // Utilities.addGithubPRTriggerForBranch(newJob, branch, checkName)
-                    } else {
-                        Utilities.addGithubPushTrigger(newJob)
-                    }
-                } else {
-                    // nonDefaultTaskSetup is e.g. DailyBuildTaskSetup (which sets up daily builds)
-                    // These jobs will only be configured for the branch specified below,
-                    // which is the name of the branch netci.groovy was processed for.
-                    // See list of such branches at:
-                    // https://github.com/dotnet/dotnet-ci/blob/master/jobs/data/repolist.txt
-                    nonDefaultTaskSetup(newJob, isPR, config)
-                }
+                CreateBuildTask(isPR, buildArch, buildType, machine, configTag, buildExtra, testExtra, runCodeAnalysis, excludeConfigIf, nonDefaultTaskSetup)
             }
         }
     }
