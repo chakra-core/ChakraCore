176be7373628271176da073ce7c5c39f1e474142
Grey Christoforo <greysAcademicCode@users.noreply.github.com>
2017-01-26 22:50:34 +0000

    jstoc.py is python2

--- bin/ch/jstoc.py
+++ bin/ch/jstoc.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 #-------------------------------------------------------------------------------------------------------
 # Copyright (C) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
