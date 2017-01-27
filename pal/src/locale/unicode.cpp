c35711b020dcc41d3a4d01e1fc1a6d9719ad7587
Mathias Bynens <mathias@qiwi.be>
2017-01-27 16:24:59 +0100

    Avoid case-sensitive file system bug
    This came up when creating the Homebrew formula: https://github.com/Homebrew/homebrew-core/pull/9323#issuecomment-275676725

--- pal/src/locale/unicode.cpp
+++ pal/src/locale/unicode.cpp
@@ -43,7 +43,7 @@ Revision History:
 #endif // __APPLE__
 #include <errno.h>
 #if HAVE_COREFOUNDATION
-#include <corefoundation/corefoundation.h>
+#include <CoreFoundation/CoreFoundation.h>
 #endif // HAVE_COREFOUNDATION
 
 #include <debugmacrosext.h>
