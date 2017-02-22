f158a4b27490eac84aeef6ff82d90e1f7daf9724
Michael Ferris <micfer@microsoft.com>
2017-02-21 18:11:47 -0800

    Update testsuite and make scripts to facilitate updating the suite in the future

---
+++ test/WasmSpec/updateTests.cmd
@@ -0,0 +1,31 @@
+::-------------------------------------------------------------------------------------------------------
+:: Copyright (C) Microsoft. All rights reserved.
+:: Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+::-------------------------------------------------------------------------------------------------------
+@echo off
+pushd %~dp0
+if not exist "%1" (
+    echo Please provide path to a chakra host, ch or jshost.
+    exit /b 1
+)
+
+if exist "wasm-spec" (
+    rd /q /s wasm-spec
+)
+git clone --depth 1 --branch master https://github.com/WebAssembly/spec.git wasm-spec
+cd wasm-spec
+git rev-parse HEAD > ..\testsuite.rev
+cd ..
+robocopy /e /mir wasm-spec\test testsuite
+rd /q /s wasm-spec
+
+rem regerate testsuite
+cd convert-test-suite
+call npm install --production
+cd ..
+node convert-test-suite --rebase %*
+git add testsuite\*
+git add testsuite-bin\*
+git add baseline\*
+echo Test updated and regerated, don't forget to update baselines
+popd
