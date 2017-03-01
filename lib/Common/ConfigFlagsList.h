50505fd8facc0628861381bca9a9669fd196b9c5
Nikolay Korovaiko <korovaikon@gmail.com>
2017-03-01 09:41:37 -0800

    make wasmfold on by default

--- lib/Common/ConfigFlagsList.h
+++ lib/Common/ConfigFlagsList.h
@@ -387,10 +387,10 @@ PHASE(All)
 #else
     #define DEFAULT_CONFIG_WasmFastArray    (false)
 #endif
-#define DEFAULT_CONFIG_WASM_FOLD          (false)
+#define DEFAULT_CONFIG_WasmFold             (true)
 #define DEFAULT_CONFIG_BgJitDelayFgBuffer   (0)
 #define DEFAULT_CONFIG_BgJitPendingFuncCap  (31)
-#define DEFAULT_CONFIG_CurrentSourceInfo     (true)
+#define DEFAULT_CONFIG_CurrentSourceInfo    (true)
 #define DEFAULT_CONFIG_CreateFunctionProxy  (true)
 #define DEFAULT_CONFIG_HybridFgJit          (false)
 #define DEFAULT_CONFIG_HybridFgJitBgQueueLengthThreshold (32)
@@ -1058,7 +1058,7 @@ FLAGPRA          (Boolean, ES6, ESSharedArrayBuffer    , sab     , "Enable Share
 #define COMPILE_DISABLE_Wasm 0
 #endif
 FLAGPR_REGOVR_EXP(Boolean, ES6, Wasm, "Enable WebAssembly", DEFAULT_CONFIG_WASM)
-FLAGNR(Boolean, WasmFold, "Enable i32/i64 const folding", DEFAULT_CONFIG_WASM_FOLD)
+FLAGNR(Boolean, WasmFold, "Enable i32/i64 const folding", DEFAULT_CONFIG_WasmFold)
 
 #ifdef ENABLE_PROJECTION
 FLAGNR(Boolean, WinRTDelegateInterfaces , "Treat WinRT Delegates as Interfaces when determining their resolvability.", DEFAULT_CONFIG_WinRTDelegateInterfaces)
