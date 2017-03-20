b542d9cca7ed3647b4202d3e4fcbafc19a12c95a
Doug Ilijev <doilij@microsoft.com>
2017-03-20 15:09:50 -0700

    Fix #2670. Set stdout in ch.exe to UTF-8 mode so console.log gets Unicode output support.
    Only use _setmode on Windows; it seems stdout is UTF-8 mode by default on Linux.
    
    After using _setmode, printf stops working. Ensure --version only uses wprintf.

--- bin/ch/ch.cpp
+++ bin/ch/ch.cpp
@@ -8,6 +8,7 @@
 #ifdef _WIN32
 #include <winver.h>
 #include <process.h>
+#include <fcntl.h>
 #endif
 
 unsigned int MessageBase::s_messageCount = 0;
@@ -129,7 +130,7 @@ void __stdcall PrintChakraCoreVersion()
                 // Doesn't matter if you are on 32 bit or 64 bit,
                 // DWORD is always 32 bits, so first two revision numbers
                 // come from dwFileVersionMS, last two come from dwFileVersionLS
-                printf("%s version %d.%d.%d.%d\n",
+                wprintf(_u("%S version %d.%d.%d.%d\n"),
                     chakraDllName,
                     (verInfo->dwFileVersionMS >> 16) & 0xffff,
                     (verInfo->dwFileVersionMS >> 0) & 0xffff,
@@ -832,6 +833,17 @@ int _cdecl wmain(int argc, __in_ecount(argc) LPWSTR argv[])
 #endif
 
 #ifdef _WIN32
+    // Set the output mode of stdout so we can display non-ASCII characters on the console and redirect to file as UTF-8
+    {
+        int result = _setmode(_fileno(stdout), _O_U8TEXT); // set stdout to UTF-8 mode
+        if (result == -1)
+        {
+            // Failed to set mode. Undefined behavior may result, so exit now.
+            wprintf(_u("Failed to set output stream mode. Exiting...\n"));
+            return EXIT_FAILURE;
+        }
+    }
+
     bool runJITServer = HandleJITServerFlag(argc, argv);
 #endif
 
