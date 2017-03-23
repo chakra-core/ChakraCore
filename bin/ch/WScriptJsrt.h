f6e35ac1fd0c34547f04f837c7b187c0c449a036
Derek Morris <demorri@microsoft.com>
2017-03-23 01:49:54 +0000

    Change WScriptJSRT LoadScript to actually free
    Now calls to it pass a finalizeCallback, which is here
    free, but could be more complex if the source text is
    differently allocated.
    
    Edit: Make calling conventions match up for free callback.
    
    The calling conventions used are dependent on platform; change to
    using a wrapper around free such that we always match the expected
    convention.
    
    Edit: Omit unneeded 'return' in free callback.

--- bin/ch/WScriptJsrt.h
+++ bin/ch/WScriptJsrt.h
@@ -87,7 +87,7 @@ public:
 #endif
 
     static bool PrintException(LPCSTR fileName, JsErrorCode jsErrorCode);
-    static JsValueRef LoadScript(JsValueRef callee, LPCSTR fileName, LPCSTR fileContent, LPCSTR scriptInjectType, bool isSourceModule);
+    static JsValueRef LoadScript(JsValueRef callee, LPCSTR fileName, LPCSTR fileContent, LPCSTR scriptInjectType, bool isSourceModule, JsFinalizeCallback finalizeCallback);
     static DWORD_PTR GetNextSourceContext();
     static JsValueRef LoadScriptFileHelper(JsValueRef callee, JsValueRef *arguments, unsigned short argumentCount, bool isSourceModule);
     static JsValueRef LoadScriptHelper(JsValueRef callee, bool isConstructCall, JsValueRef *arguments, unsigned short argumentCount, void *callbackState, bool isSourceModule);
@@ -95,6 +95,7 @@ public:
 private:
     static bool CreateArgumentsObject(JsValueRef *argsObject);
     static bool CreateNamedFunction(const char*, JsNativeFunction callback, JsValueRef* functionVar);
+    static void FinalizeFree(void * addr);
     static JsValueRef CALLBACK EchoCallback(JsValueRef callee, bool isConstructCall, JsValueRef *arguments, unsigned short argumentCount, void *callbackState);
     static JsValueRef CALLBACK QuitCallback(JsValueRef callee, bool isConstructCall, JsValueRef *arguments, unsigned short argumentCount, void *callbackState);
     static JsValueRef CALLBACK LoadScriptFileCallback(JsValueRef callee, bool isConstructCall, JsValueRef *arguments, unsigned short argumentCount, void *callbackState);
