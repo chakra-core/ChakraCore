365aea7b6753fa7be95adef45a6cf0b5462f46ed
Oguz Bastemur <ogbastem@microsoft.com>
2017-04-28 16:01:34 +0200

    ISF-Process: condition-less
    In case `!ehBailoutData` it is unlikely the other 2 comparisons will
    result true. However, other two may result true but  actually we may
    not have `ehBailoutData`. So, condition less.

--- lib/Runtime/Language/InterpreterStackFrame.cpp
+++ lib/Runtime/Language/InterpreterStackFrame.cpp
@@ -3365,9 +3365,9 @@ namespace Js
         } autoRestore(this);
 #endif
 
-        if ((m_flags & Js::InterpreterStackFrameFlags_FromBailOut) && !(m_flags & InterpreterStackFrameFlags_ProcessingBailOutFromEHCode))
+        if (this->ehBailoutData)
         {
-            if (this->ehBailoutData)
+            if ((m_flags & Js::InterpreterStackFrameFlags_FromBailOut) && !(m_flags & InterpreterStackFrameFlags_ProcessingBailOutFromEHCode))
             {
                 m_flags |= Js::InterpreterStackFrameFlags_ProcessingBailOutFromEHCode;
                 EHBailoutData * topLevelEHBailoutData = this->ehBailoutData;
@@ -3381,6 +3381,7 @@ namespace Js
                 this->ehBailoutData = nullptr;
             }
         }
+
 #ifdef ASMJS_PLAT
         if( GetFunctionBody()->GetIsAsmjsMode() )
         {
