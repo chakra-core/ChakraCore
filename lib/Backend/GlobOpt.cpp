e45b48f9f4244c4bfc31e446550fb2462fab8cfc
Derek Morris <demorri@microsoft.com>
2017-05-10 20:11:37 -0700

    Add a globOpt pointer to GlobOptBlockData
    This is somewhat necessary for some of the upcoming changes, since
    the code will need to access some GlobOpt globals from outside of
    the actual GlobOpt class (since a few things are information about
    the pass, or information about the optimizations enabled).

--- lib/Backend/GlobOpt.cpp
+++ lib/Backend/GlobOpt.cpp
@@ -178,6 +178,7 @@ public:
 
     PREVENT_COPY(AutoRestoreVal);
 };
+
 GlobOpt::GlobOpt(Func * func)
     : func(func),
     intConstantToStackSymMap(nullptr),
@@ -535,8 +536,6 @@ GlobOpt::ForwardPass()
 
     END_CODEGEN_PHASE(this->func, Js::ForwardPhase);
 }
-
-
 
 void
 GlobOpt::OptBlock(BasicBlock *block)
@@ -1488,6 +1487,8 @@ GlobOpt::MergePredBlocksValueMaps(BasicBlock *block)
 void
 GlobOpt::NulloutBlockData(GlobOptBlockData *data)
 {
+    data->globOpt = this;
+
     data->symToValueMap = nullptr;
     data->exprToValueMap = nullptr;
     data->liveFields = nullptr;
@@ -1527,6 +1528,8 @@ GlobOpt::NulloutBlockData(GlobOptBlockData *data)
 void
 GlobOpt::InitBlockData(GlobOptBlockData* data)
 {
+    data->globOpt = this;
+
     JitArenaAllocator *const alloc = this->alloc;
 
     data->symToValueMap = GlobHashTable::New(alloc, 64);
@@ -1574,6 +1577,8 @@ GlobOpt::InitBlockData(GlobOptBlockData* data)
 void
 GlobOpt::ReuseBlockData(GlobOptBlockData *toData, GlobOptBlockData *fromData)
 {
+    toData->globOpt = fromData->globOpt;
+
     // Reuse dead map
     toData->symToValueMap = fromData->symToValueMap;
     toData->exprToValueMap = fromData->exprToValueMap;
@@ -1625,6 +1630,8 @@ GlobOpt::ReuseBlockData(GlobOptBlockData *toData, GlobOptBlockData *fromData)
 void
 GlobOpt::CopyBlockData(GlobOptBlockData *toData, GlobOptBlockData *fromData)
 {
+    toData->globOpt = fromData->globOpt;
+
     toData->symToValueMap = fromData->symToValueMap;
     toData->exprToValueMap = fromData->exprToValueMap;
     toData->liveFields = fromData->liveFields;
@@ -1670,6 +1677,8 @@ void GlobOpt::CloneBlockData(BasicBlock *const toBlock, GlobOptBlockData *const 
 {
     GlobOptBlockData *const fromData = &fromBlock->globOptData;
     JitArenaAllocator *const alloc = this->alloc;
+
+    toData->globOpt = fromData->globOpt;
 
     toData->symToValueMap = fromData->symToValueMap->Copy();
     toData->exprToValueMap = fromData->exprToValueMap->Copy();
