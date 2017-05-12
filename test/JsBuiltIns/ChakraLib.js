ec12a406c0d19c6561b43cf5d8e706c5fdba55e4
Etienne BAUDOUX <t-etbau@microsoft.com>
2017-05-12 11:45:31 -0700

    - Unit test added. - Refactoring

--- test/JsBuiltIns/ChakraLib.js
+++ test/JsBuiltIns/ChakraLib.js
@@ -10,6 +10,13 @@ var tests = [
         name: "Access to __chakraLibrary work",
         body: function () {
             assert.isNotUndefined(__chakraLibrary, "__chakraLibrary must NOT be undefined as we're using the -LdChakraLib flag.");
+        },
+        name: "Overriding __chakraLibrary must not work change the behavior.",
+        body: function () {
+            __chakraLibrary = -1;
+            var array = [1, 2, 3, 4];
+            assert.areNotEqual(__chakraLibrary, -1, "__chakraLibrary must not be equal to -1 as the -LdChakraLib flag force to use the internal library associated to the __chakraLibrary token.");
+            assert.areEqual(array.indexOf(4), 3, "The __chakraLibrary in the indexOf method must give access to built ins methods instead of -1.");
         }
     }
 ];
--- test/JsBuiltIns/ChakraLib_disabled.js
+++ test/JsBuiltIns/ChakraLib_disabled.js
@@ -16,7 +16,7 @@ var tests = [
             var __chakraLibrary = -1;
             var array = [1, 2, 3, 4];
             assert.areEqual(__chakraLibrary, -1, "__chakraLibrary must be -1 outside of the BuiltIns methods.");
-            assert.areEqual(array.indexOf(1), 0, "The __chakraLibrary in the indexOf method must give access to built ins methods instead of -1.");
+            assert.areEqual(array.indexOf(4), 3, "The __chakraLibrary in the indexOf method must give access to built ins methods instead of -1.");
         }
     }
 ];
