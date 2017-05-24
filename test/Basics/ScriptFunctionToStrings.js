ee986f49bbf10392f3f4bd1dd7f30bbf4965cda0
Doug Ilijev <doilij@microsoft.com>
2017-05-24 15:38:16 -0700

    ScriptFunctionToStrings.js: re-enabled test except for failing test case; fixed formatting; remove on-by-default test flags.

--- test/Basics/ScriptFunctionToStrings.js
+++ test/Basics/ScriptFunctionToStrings.js
@@ -21,9 +21,8 @@ var tests = [
         {
             eval("var a = function \t\n\r\t foo() {var a = 5;}");
             assert.areEqual("function foo() {var a = 5;}", a.toString(), "toString should remove all extra whitespace, new lines, tabs and carriage return before the open (");
-            a = function(i) {i++;}
-            assert.areEqual("function (i) {i++;}", a.toString(), "toString should add a space if one does not exist");
-
+            a = function (i) { i++; }
+            assert.areEqual("function (i) { i++; }", a.toString(), "toString should add a space if one does not exist");
         }
     },
     {
@@ -50,67 +49,81 @@ var tests = [
         name: "Named function expression tests",
         body: function ()
         {
-             eval("var o = { foo : function \n\t bar \t () {}}");
-             eval("o.e = function \t qui \t () {}");
-             assert.areEqual("function bar() {}",o.foo.toString(),"confirm that the foo identifier does not override the name bar ");
-             assert.areEqual("function qui() {}",o.e.toString(),"confirm that the foo identifier does not override the name qui");
+            eval("var o = { foo : function \n\t bar \t () {}}");
+            eval("o.e = function \t qui \t () {}");
+            assert.areEqual("function bar() {}", o.foo.toString(), "confirm that the foo identifier does not override the name bar ");
+            assert.areEqual("function qui() {}", o.e.toString(), "confirm that the foo identifier does not override the name qui");
         }
-   },
-   {
+    },
+    {
         name: "function expression tests without names",
         body: function ()
         {
-             eval("var o = { foo : function \n\t  \t () {}}");
-             eval("o.e = function \t  \t () {}");
-             assert.areEqual("function () {}",o.foo.toString(),"confirm that the foo identifier does not override the name bar ");
-             assert.areEqual("function () {}",o.e.toString(),"confirm that the foo identifier does not override the name qui");
+            eval("var o = { foo : function \n\t  \t () {}}");
+            eval("o.e = function \t  \t () {}");
+            assert.areEqual("function () {}", o.foo.toString(), "confirm that the foo identifier does not override the name bar ");
+            assert.areEqual("function () {}", o.e.toString(), "confirm that the foo identifier does not override the name qui");
         }
-   },
-   {
+    },
+    {
         name: "internal function test",
         body: function ()
         {
-             eval("function foo() { return foo.toString(); }");
-             var a = foo;
-             assert.areEqual("function foo() { return foo.toString(); }", a(),"confirm that even if we call toString internally it has no effect on the name")
+            eval("function foo() { return foo.toString(); }");
+            var a = foo;
+            assert.areEqual("function foo() { return foo.toString(); }", a(), "confirm that even if we call toString internally it has no effect on the name")
         }
-   },
-   {
+    },
+    {
         name: "class method test",
         body: function ()
         {
-             eval("var qux = class { constructor(){} static func(){} method(){} get getter(){} set setter(v){}}");
-             var quxObj = new qux();
-             assert.areEqual("constructor(){}",quxObj.constructor.toString(),"The constructor should have the toString  with name constructor");
-             assert.areEqual("func(){}",qux.func.toString(),"the name should be func")
-             assert.areEqual("method(){}",quxObj.method.toString(),"the name should be method")
+            eval("var qux = class { constructor(){} static func(){} method(){} get getter(){} set setter(v){}}");
+            var quxObj = new qux();
+            assert.areEqual("func(){}", qux.func.toString(), "the name should be func")
+            assert.areEqual("method(){}", quxObj.method.toString(), "the name should be method")
 
-             var oGet = Object.getOwnPropertyDescriptor(qux.prototype,"getter");
-             var oSet = Object.getOwnPropertyDescriptor(qux.prototype,"setter");
-             assert.areEqual("getter(){}",  oGet.get.toString(), "the name should be getter");
-             assert.areEqual("setter(v){}", oSet.set.toString(), "the name should be setter");
-             var qux = class {};
-             var quxObj = new qux();
-             // I left a space between closing ellipse ) and the opening bracket { because that's how all browsers do toString on runtime functions
-             // Should this not be how other browsers do default constructor go to RuntimeCommon.h and change JS_DEFAULT_CTOR_DISPLAY_STRING
-             assert.areEqual("constructor() {}",quxObj.constructor.toString(),"The constructor should have the toString with name constructor")
+            var oGet = Object.getOwnPropertyDescriptor(qux.prototype, "getter");
+            var oSet = Object.getOwnPropertyDescriptor(qux.prototype, "setter");
+            assert.areEqual("getter(){}", oGet.get.toString(), "the name should be getter");
+            assert.areEqual("setter(v){}", oSet.set.toString(), "the name should be setter");
         }
-   },
-   {
+    },
+    /* TODO fix and re-enable this test case
+    {
+        name: "class constructor test",
+        body: function ()
+        {
+            eval("var qux = class { constructor(){} static func(){} method(){} get getter(){} set setter(v){}}");
+            var quxObj = new qux();
+            // FIXME Test threw exception: assert.areEqual failed: expected: constructor(){} actual: class { constructor(){} static func(){} method(){} get getter(){} set setter(v){}}
+            assert.areEqual("constructor(){}", quxObj.constructor.toString(), "The constructor should have the toString  with name constructor");
+
+            var qux = class { };
+            var quxObj = new qux();
+            // I left a space between closing ellipse ) and the opening bracket { because that's how all browsers do toString on runtime functions
+            // Should this not be how other browsers do default constructor go to RuntimeCommon.h and change JS_DEFAULT_CTOR_DISPLAY_STRING
+            // FIXME Test threw exception: assert.areEqual failed: expected: constructor() {} actual: class { }
+            assert.areEqual("constructor() {}", quxObj.constructor.toString(), "The constructor should have the toString with name constructor")
+        }
+    },
+    */
+    {
         name: "shorthand method function test",
         body: function ()
         {
-             var o = {['f']() {},g () {}};
-             assert.areEqual("f() {}",o.f.toString());
+            var o = { ['f']() { }, g() { } };
+            assert.areEqual("f() { }", o.f.toString());
         }
-   },
-   {
+    },
+    {
         name: "arrow function Test",
         body: function ()
         {
-             var arrowDecl = () => {};
-             assert.areEqual("() => {}",arrowDecl.toString(),"Make sure arrow functions remain unaffected by ie12 formatting");
+            var arrowDecl = () => { };
+            assert.areEqual("() => { }", arrowDecl.toString(), "Make sure arrow functions remain unaffected by ie12 formatting");
         }
-   }
+    }
 ];
+
 testRunner.runTests(tests, { verbose: WScript.Arguments[0] != "summary" });
