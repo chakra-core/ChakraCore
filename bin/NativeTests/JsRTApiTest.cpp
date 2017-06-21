2635e920a3b88bf13893cb537efe1a0aaeddfd0c
Oguz Bastemur <ogbastem@microsoft.com>
2017-06-21 14:20:51 +0200

    jsrt: don't check runtime on each API call for non-browser
    JSRT doesn't know how host is using the API. Verifying runtime state on each call redundant and time taking.
    
    Simple JsHasException call returns all the useful stuff about runtime when it is needed.

--- bin/NativeTests/JsRTApiTest.cpp
+++ bin/NativeTests/JsRTApiTest.cpp
@@ -1741,9 +1741,12 @@ namespace JsRTApiTest
             REQUIRE(JsIsRuntimeExecutionDisabled(runtime, &isDisabled) == JsNoError);
             CHECK(isDisabled);
 
-
+#ifdef NTBUILD
             REQUIRE(JsCallFunction(postScriptAbortFunction, args, 1, nullptr) == JsErrorInDisabledState);
-
+#else // !JSRT_VERIFY_RUNTIME_STATE
+            bool hasException = false;
+            REQUIRE(JsHasException(&hasException) == JsErrorInDisabledState);
+#endif
             REQUIRE(JsGetAndClearException(&exception) == JsErrorInDisabledState);
             REQUIRE(JsEnableRuntimeExecution(runtime) == JsNoError);
             threadArgs.EndScriptExecution();
@@ -1787,7 +1790,12 @@ namespace JsRTApiTest
             bool isDisabled;
             REQUIRE(JsIsRuntimeExecutionDisabled(runtime, &isDisabled) == JsNoError);
             CHECK(isDisabled);
+#ifdef NTBUILD
             REQUIRE(JsRunScript(terminationTests[i], JS_SOURCE_CONTEXT_NONE, _u(""), &result) == JsErrorInDisabledState);
+#else // !JSRT_VERIFY_RUNTIME_STATE
+            bool hasException = false;
+            REQUIRE(JsHasException(&hasException) == JsErrorInDisabledState);
+#endif
             REQUIRE(JsGetAndClearException(&exception) == JsErrorInDisabledState);
             REQUIRE(JsEnableRuntimeExecution(runtime) == JsNoError);
             threadArgs.EndScriptExecution();
