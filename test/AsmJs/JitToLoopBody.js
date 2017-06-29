42222cb5fb706eaca5cace6990a8e9cdc623eeda
Michael Ferris <micfer@microsoft.com>
2017-06-29 15:14:21 -0700

    Fix how we check if the last asm.js stmt is a return and throw if the type doesn't match Fixes OS#9592312

--- test/AsmJs/JitToLoopBody.js
+++ test/AsmJs/JitToLoopBody.js
@@ -3,10 +3,10 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 
-  // return double 
+  // return double
   // do while and while loops
   function AsmModule() {
-    "use asm";      
+    "use asm";
     var x1 = 10;
     function f3(x,y){
         x = x|0;
@@ -25,28 +25,27 @@
                     if((n|0) > 50)
                         return +y;
                     x = (x+1)|0;
-                    y = +(y * z) 
+                    y = +(y * z)
                     n = (n + 1)|0;
                 }while((n|0) < 100)
-            }            
+            }
         }
         return +y;
     }
-    
+
     function bar(k)
     {
         k = k|0;
-        if( (k|0) > 5) 
-            return +f3(1,1.5);  
-        else 
-            return 1.5;
+        if( (k|0) > 5)
+            return +f3(1,1.5);
+        return 1.5;
     }
-    
+
     return bar
 }
 
 var bar = AsmModule();
-print(bar(1))  
-print(bar(1))   
-print(bar(10))   
-print(bar(10))   
+print(bar(1))
+print(bar(1))
+print(bar(10))
+print(bar(10))
