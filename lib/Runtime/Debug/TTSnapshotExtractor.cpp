5f5f2fbf2168286a73b989a3258c5d4dc2c94cef
Atul Katti <akatti@microsoft.com>
2017-07-07 15:16:33 -0700

    Fixes #2471 Symbol registration map should support null characters in symbol names.

--- lib/Runtime/Debug/TTSnapshotExtractor.cpp
+++ lib/Runtime/Debug/TTSnapshotExtractor.cpp
@@ -404,7 +404,7 @@ namespace TTD
         }
 
         //extract the thread context symbol map info
-        JsUtil::BaseDictionary<const char16*, const Js::PropertyRecord*, Recycler>* tcSymbolRegistrationMap = threadContext->GetSymbolRegistrationMap_TTD();
+        JsUtil::BaseDictionary<Js::HashedCharacterBuffer<char16>*, const Js::PropertyRecord*, Recycler, PowerOf2SizePolicy, Js::PropertyRecordStringHashComparer>* tcSymbolRegistrationMap = threadContext->GetSymbolRegistrationMap_TTD();
         UnorderedArrayList<Js::PropertyId, TTD_ARRAY_LIST_SIZE_XSMALL>& tcSymbolMapInfo = this->m_pendingSnap->GetTCSymbolMapInfoList();
         for(auto iter = tcSymbolRegistrationMap->GetIterator(); iter.IsValid(); iter.MoveNext())
         {
