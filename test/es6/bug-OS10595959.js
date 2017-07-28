ee95ce05b6bf6c3ec9fbece4e6adec88724c8296
Rajat Dua <radua@microsoft.com>
2017-07-28 15:09:18 -0700

    OS#10595959 : Ensure that LdHeapArguments is the first thing in the bytecode after recording constants

---
+++ test/es6/bug-OS10595959.js
@@ -0,0 +1,19 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
+class BaseClass {
+}
+var FloatArr0 = [];
+var VarArr0 = Array();
+class class1 extends BaseClass {
+  constructor(argMath24 = 1577048671 ? f32[(e /= arguments[15] ? func0.call(arrObj0) : 'caller') & 255] : typeof a == 'string') {
+    super();
+  }
+}
+for (var v3 = 0; v3<10; v3++) {
+  FloatArr0.push(new class1(VarArr0, '$M+!,($9($.)!wm'));
+}
+
+print("Passed");
