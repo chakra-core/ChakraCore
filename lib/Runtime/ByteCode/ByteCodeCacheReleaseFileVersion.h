a5bcd446d869b8f45cc97cd20eacf560eeee65ba
Rajat Dua <radua@microsoft.com>
2017-08-04 16:53:59 -0700

    Intl bytecodes and bytecode cache guid update

--- lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
+++ lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
@@ -4,6 +4,6 @@
 //-------------------------------------------------------------------------------------------------------
 // NOTE: If there is a merge conflict the correct fix is to make a new GUID.
 
-// {019e81e7-4db4-498b-938a-b64c43512c81}
+// {8C72FFDA-CA52-4A71-AF70-B96C7BFEEEAD}
 const GUID byteCodeCacheReleaseFileVersion =
-{ 0x019e81e7, 0x4db4, 0x498b, { 0x93, 0x8a, 0xb6, 0x4c, 0x43, 0x51, 0x2c, 0x81 } };
+{ 0x8C72FFDA, 0xCA52, 0x4A71, { 0xAF, 0x70, 0xB9, 0x6C, 0x7B, 0xFE, 0xEE, 0xAD } };
