f32245f05db7b35ee4691d025c834d98e626ef57
Richard Cobbe <ricobbe@microsoft.com>
2017-08-08 15:15:09 -0700

    Add unit test for stack-alloc closure copy-prop bug

---
+++ test/Closures/copy-prop-stack-slot.js
@@ -0,0 +1,21 @@
+﻿WScript.LoadScriptFile('copy-prop-stack-slot-test-framework.js');
+var tc=new TestCase();
+tc.id="38";
+tc.desc="Enumerate arguments";
+tc.test=function(){	
+    var actualContent = "";
+	var actualIndex = "";
+
+	function testArgument(){
+		for(var a in arguments){
+            actualContent += arguments[a];
+			actualIndex += a;
+		}
+	}
+
+	testArgument(12,13,14,15);			    
+	WScript.Echo(actualContent);
+	WScript.Echo(actualIndex);
+}
+tc.AddTest();
+Run();
