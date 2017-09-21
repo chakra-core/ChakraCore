255c910cae242a828ac2c04aade7317243c14bcf
Michael Ferris <micfer@microsoft.com>
2017-09-21 15:36:26 -0700

    Implement generic way of dealing with prefixes OS#14733708 : Do not allow Tracing opcode from being read by the binary reader.

---
+++ test/wasm/tracing_regress.js
@@ -0,0 +1,26 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft Corporation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
+WScript.LoadScriptFile("../wasmspec/testsuite/harness/wasm-constants.js");
+WScript.LoadScriptFile("../wasmspec/testsuite/harness/wasm-module-builder.js");
+
+const tracingPrefix = 0xf0;
+const builder = new WasmModuleBuilder();
+const typeIndex = builder.addType(kSig_v_v);
+builder.addFunction("foo", typeIndex).addBody([
+  tracingPrefix, 0x0,
+]).exportFunc();
+
+try {
+  const {exports: {foo}} = new WebAssembly.Instance(new WebAssembly.Module(builder.toBuffer()));
+  foo();
+  console.log("Failed: Tracing prefix should not be accepted");
+} catch (e) {
+  if (e.message.includes("Tracing opcodes not allowed")) {
+    console.log("pass");
+  } else {
+    console.log("Wrong error: " + e);
+  }
+}
