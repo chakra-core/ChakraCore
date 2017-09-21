74f68b70da41fc848d1ed6bee7e60479563c20c7
Jack Horton <jahorto@microsoft.com>
2017-09-21 15:02:32 -0700

    Initial stab at ICU implementation of getDefaultLocale

--- lib/Runtime/Library/IntlEngineInterfaceExtensionObject.cpp
+++ lib/Runtime/Library/IntlEngineInterfaceExtensionObject.cpp
@@ -723,21 +723,13 @@ namespace Js
 
         if (
 #if defined(INTL_WINGLOB)
-            // XPLAT-TODO (doilij): Implement this in PlatformAgnostic
             GetUserDefaultLocaleName(defaultLocale, _countof(defaultLocale)) == 0
 #else
             IcuIntlAdapter::GetUserDefaultLocaleName(defaultLocale, _countof(defaultLocale)) == 0
 #endif
             || defaultLocale[0] == '\0')
         {
-#if defined(INTL_WINGLOB)
             return scriptContext->GetLibrary()->GetUndefined();
-#else
-#ifdef INTL_ICU_DEBUG
-            Output::Print(_u("EntryIntl_GetDefaultLocale > IcuIntlAdapter::GetUserDefaultLocaleName returned 0: returning null to fallback to JS\n"));
-#endif
-            return scriptContext->GetLibrary()->GetNull();
-#endif
         }
 
         return JavascriptString::NewCopySz(defaultLocale, scriptContext);
