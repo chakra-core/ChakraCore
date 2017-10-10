d7e9d0d6495a9b7bc36c497bef378db961983fcf
Sandeep Agarwal <saagarwa@microsoft.com>
2017-10-10 10:25:50 -0700

    Add copyright notice to test case

--- test/switchStatement/aggressiveintoff.js
+++ test/switchStatement/aggressiveintoff.js
@@ -1,3 +1,8 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
 function opt() {
     for (let i = 0; i < 100; i++) {
         let j = i - 2;
