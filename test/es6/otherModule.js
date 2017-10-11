9db733fd4e5aab2d3bac32192f39443dbe419e47
rhuanjl <rhuan.l@gmail.com>
2017-10-11 23:14:28 +0100

    Notify host Dynamic Module ready
    Previously the call to notify the Host that a module was ready was made only for:
    a) Errors AND
    b) Parent level modules
    
    A module loaded with a dynamic import may not be a parent and hopefully won't always be an error - this condition change means that the notification is made for any dynamic import when completed.
    
    Also adjusts a test bug_issue_3257 which was failing to find two files - this failure was hidden by not notifying the host when a dynamic module was ready as the dynamic imports that should have failed were acting as no-ops.
    
    Also adds a new test to ensure that dynamic imports from modules do notify the host.

---
+++ test/es6/otherModule.js
@@ -0,0 +1,8 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
+//test that dynamic import works from module
+//this file is imported only once - dynamicly from another module to check that the host is notified for that
+export const success = true;
