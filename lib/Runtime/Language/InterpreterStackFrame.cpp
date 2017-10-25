7c5817e5b3ef24760f951bd8c9a50f9d6fb2665d
Arun Purushan (Intel Americas Inc) <b-arpuru@microsoft.com>
2017-10-25 13:25:43 -0700

    i

--- lib/Runtime/Language/InterpreterStackFrame.cpp
+++ lib/Runtime/Language/InterpreterStackFrame.cpp
@@ -3802,7 +3802,7 @@ namespace Js
         case AsmJsRetType::Uint32x4:
         case AsmJsRetType::Uint16x8:
         case AsmJsRetType::Uint8x16:
-#if _M_X64 && defined(_WIN32)
+#if _M_X64 && defined(_WIN64)
             X86SIMDValue simdVal;
             simdVal.m128_value = JavascriptFunction::CallAsmJsFunction<__m128>(function, entrypointInfo->jsMethod, m_outParams, alignedArgsSize, reg);
             m_localSimdSlots[returnReg] = X86SIMDValue::ToSIMDValue(simdVal);
