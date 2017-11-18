b251ed877a206cb8c607cde3e55f5504445a3131
Sandeep Agarwal <saagarwa@microsoft.com>
2017-11-17 16:47:22 -0800

    ARM64: SaveAllRegisters during bailout should use x17 instead of x16

--- lib/Backend/arm64/LinearScanMdA.asm
+++ lib/Backend/arm64/LinearScanMdA.asm
@@ -29,41 +29,42 @@
     ; lr == return address
 
     ; Save all registers except the above, which would have already been saved by jitted code if necessary
-    ldr x16, [x0] ; bailOutRecord->globalBailOutRecordDataTable
-    ldr x16, [x16] ; bailOutRecord->globalBailOutRecordDataTable->registerSaveSpace
-    str x1, [x16, #1*8]
-    stp x2, x3, [x16, #2*8]
-    stp x4, x5, [x16, #4*8]
-    stp x6, x7, [x16, #6*8]
-    stp x8, x9, [x16, #8*8]
-    stp x10, x11, [x16, #10*8]
-    stp x12, x13, [x16, #12*8]
-    stp x14, x15, [x16, #14*8]
-    ; skip x16/x17/x18
-    stp x19, x20, [x16, #19*8]
-    stp x21, x22, [x16, #21*8]
-    stp x23, x24, [x16, #23*8]
-    stp x25, x26, [x16, #25*8]
-    stp x27, x28, [x16, #27*8]
-    str fp, [x16, #29*8]
+    ldr x17, [x0] ; bailOutRecord->globalBailOutRecordDataTable
+    ldr x17, [x17] ; bailOutRecord->globalBailOutRecordDataTable->registerSaveSpace
+    str x1, [x17, #1*8]
+    stp x2, x3, [x17, #2*8]
+    stp x4, x5, [x17, #4*8]
+    stp x6, x7, [x17, #6*8]
+    stp x8, x9, [x17, #8*8]
+    stp x10, x11, [x17, #10*8]
+    stp x12, x13, [x17, #12*8]
+    stp x14, x15, [x17, #14*8]
+    str x16, [x17, #16*8]
+    ; skip x17/x18
+    stp x19, x20, [x17, #19*8]
+    stp x21, x22, [x17, #21*8]
+    stp x23, x24, [x17, #23*8]
+    stp x25, x26, [x17, #25*8]
+    stp x27, x28, [x17, #27*8]
+    str fp, [x17, #29*8]
     ; skip lr, sp, zr
-    add x16, x16, #33*8
-    stp d0, d1, [x16, #0*8]
-    stp d2, d3, [x16, #2*8]
-    stp d4, d5, [x16, #4*8]
-    stp d6, d7, [x16, #6*8]
-    stp d8, d9, [x16, #8*8]
-    stp d10, d11, [x16, #10*8]
-    stp d12, d13, [x16, #12*8]
-    stp d14, d15, [x16, #14*8]
-    stp d16, d17, [x16, #16*8]
-    stp d18, d19, [x16, #18*8]
-    stp d20, d21, [x16, #20*8]
-    stp d22, d23, [x16, #22*8]
-    stp d24, d25, [x16, #24*8]
-    stp d26, d27, [x16, #26*8]
-    stp d28, d29, [x16, #28*8]
-    ;stp d30, d31, [x16, #30*8]
+    add x17, x17, #33*8
+    stp d0, d1, [x17, #0*8]
+    stp d2, d3, [x17, #2*8]
+    stp d4, d5, [x17, #4*8]
+    stp d6, d7, [x17, #6*8]
+    stp d8, d9, [x17, #8*8]
+    stp d10, d11, [x17, #10*8]
+    stp d12, d13, [x17, #12*8]
+    stp d14, d15, [x17, #14*8]
+    stp d16, d17, [x17, #16*8]
+    stp d18, d19, [x17, #18*8]
+    stp d20, d21, [x17, #20*8]
+    stp d22, d23, [x17, #22*8]
+    stp d24, d25, [x17, #24*8]
+    stp d26, d27, [x17, #26*8]
+    stp d28, d29, [x17, #28*8]
+    ;stp d30, d31, [x17, #30*8]
 
     b |?BailOut@BailOutRecord@@SAPEAXPEBV1@@Z|
 
@@ -79,40 +80,41 @@
     ; lr == return address
 
     ; Save all registers except the above, which would have already been saved by jitted code if necessary
-    ldr x16, [x0] ; bailOutRecord->globalBailOutRecordDataTable
-    ldr x16, [x16] ; bailOutRecord->globalBailOutRecordDataTable->registerSaveSpace
-    stp x2, x3, [x16, #2*8]
-    stp x4, x5, [x16, #4*8]
-    stp x6, x7, [x16, #6*8]
-    stp x8, x9, [x16, #8*8]
-    stp x10, x11, [x16, #10*8]
-    stp x12, x13, [x16, #12*8]
-    stp x14, x15, [x16, #14*8]
-    ; skip x16/x17/x18
-    stp x19, x20, [x16, #19*8]
-    stp x21, x22, [x16, #21*8]
-    stp x23, x24, [x16, #23*8]
-    stp x25, x26, [x16, #25*8]
-    stp x27, x28, [x16, #27*8]
-    str fp, [x16, #29*8]
+    ldr x17, [x0] ; bailOutRecord->globalBailOutRecordDataTable
+    ldr x17, [x17] ; bailOutRecord->globalBailOutRecordDataTable->registerSaveSpace
+    stp x2, x3, [x17, #2*8]
+    stp x4, x5, [x17, #4*8]
+    stp x6, x7, [x17, #6*8]
+    stp x8, x9, [x17, #8*8]
+    stp x10, x11, [x17, #10*8]
+    stp x12, x13, [x17, #12*8]
+    stp x14, x15, [x17, #14*8]
+    str x16, [x17, #16*8]
+    ; skip x17/x18
+    stp x19, x20, [x17, #19*8]
+    stp x21, x22, [x17, #21*8]
+    stp x23, x24, [x17, #23*8]
+    stp x25, x26, [x17, #25*8]
+    stp x27, x28, [x17, #27*8]
+    str fp, [x17, #29*8]
     ; skip lr, sp, zr
-    add x16, x16, #33*8
-    stp d0, d1, [x16, #0*8]
-    stp d2, d3, [x16, #2*8]
-    stp d4, d5, [x16, #4*8]
-    stp d6, d7, [x16, #6*8]
-    stp d8, d9, [x16, #8*8]
-    stp d10, d11, [x16, #10*8]
-    stp d12, d13, [x16, #12*8]
-    stp d14, d15, [x16, #14*8]
-    stp d16, d17, [x16, #16*8]
-    stp d18, d19, [x16, #18*8]
-    stp d20, d21, [x16, #20*8]
-    stp d22, d23, [x16, #22*8]
-    stp d24, d25, [x16, #24*8]
-    stp d26, d27, [x16, #26*8]
-    stp d28, d29, [x16, #28*8]
-    ;stp d30, d31, [x16, #30*8]
+    add x17, x17, #33*8
+    stp d0, d1, [x17, #0*8]
+    stp d2, d3, [x17, #2*8]
+    stp d4, d5, [x17, #4*8]
+    stp d6, d7, [x17, #6*8]
+    stp d8, d9, [x17, #8*8]
+    stp d10, d11, [x17, #10*8]
+    stp d12, d13, [x17, #12*8]
+    stp d14, d15, [x17, #14*8]
+    stp d16, d17, [x17, #16*8]
+    stp d18, d19, [x17, #18*8]
+    stp d20, d21, [x17, #20*8]
+    stp d22, d23, [x17, #22*8]
+    stp d24, d25, [x17, #24*8]
+    stp d26, d27, [x17, #26*8]
+    stp d28, d29, [x17, #28*8]
+    ;stp d30, d31, [x17, #30*8]
 
     b |?BailOut@BranchBailOutRecord@@SAPEAXPEBV1@H@Z|
 
