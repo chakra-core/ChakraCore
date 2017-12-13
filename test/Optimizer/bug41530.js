0ea8624124f86815a9e210dddfbff1cb3fe50670
Paul Leathers <pleath@microsoft.com>
2017-12-12 18:20:13 -0800

    [CVE-2018-0769] JIT: Incorrect bounds calculation - Google, Inc.

---
+++ test/Optimizer/bug41530.js
@@ -0,0 +1,21 @@
+function opt(arr) {
+    if (arr.length <= 15)
+        return;
+
+    let j = 0;
+    for (let i = 0; i < 2; i++) {
+        arr[j] = 0x1234; // (a)
+        j += 0x100000;
+        j + 0x7ffffff0;
+    }
+}
+
+function main() {
+    for (let i = 0; i < 0x10000; i++) {
+        opt(new Uint32Array(100));
+    }
+}
+
+main();
+
+WScript.Echo('pass');
