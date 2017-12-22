ffa75cb98deac75c0b664d42ef5500a409908978
Michael Ferris <micfer@microsoft.com>
2017-12-22 20:58:44 +0000

    Fix PAL implementation of _vsnwprintf & _vsnprintf with regards to truncation. Fix some unit tests to be able to run in xplat builds (mostly problems with filename casing) Use HeapNew & HeapDelete for exception message in Wasm. Make test regress.js more generic to be able to add more regression tests there.

--- lib/Common/Memory/HeapAllocator.h
+++ lib/Common/Memory/HeapAllocator.h
@@ -93,6 +93,57 @@ struct HeapAllocatorData
 
 struct HeapAllocator
 {
+    template<typename T>
+    struct AutoFree
+    {
+    private:
+        T * obj = nullptr;
+    public:
+        ~AutoFree()
+        {
+            HeapDelete(obj);
+        }
+        void Replace(T* obj)
+        {
+            this->obj = obj;
+        }
+        void Release()
+        {
+            this->obj = nullptr;
+        }
+        T* Get() const
+        {
+            return this->obj;
+        }
+    };
+
+    template<typename T>
+    struct AutoFreeArray
+    {
+    private:
+        T* obj = nullptr;
+        size_t count = 0;
+    public:
+        ~AutoFreeArray()
+        {
+            HeapDeleteArray(count, obj);
+        }
+        void Replace(__ecount(count) T* obj, size_t count)
+        {
+            this->obj = obj;
+            this->count = count;
+        }
+        void Release()
+        {
+            this->obj = nullptr;
+            this->count = 0;
+        }
+        T* Get() const
+        {
+            return this->obj;
+        }
+    };
+
     static const bool FakeZeroLengthArray = false;
 
     char * Alloc(DECLSPEC_GUARD_OVERFLOW size_t byteSize)
