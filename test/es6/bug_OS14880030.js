db195070e9599a6d7dd69cc023be168fc31633c8
Taylor Woll <tawoll@ntdev.microsoft.com>
2018-01-03 14:00:21 -0800

    Update test file to use unittestframework

--- test/es6/bug_OS14880030.js
+++ test/es6/bug_OS14880030.js
@@ -6,54 +6,93 @@
 "use strict";
 var r = delete this;
 
-function test1() {
-    "use strict";
-    return delete this;
-}
+WScript.LoadScriptFile("..\\UnitTestFramework\\UnitTestFramework.js");
 
-function test2() {
-    "use strict";
-    return delete new.target;
-}
-
-function test3() {
-    "use strict";
-    try {
-        eval('delete arguments;');
-    } catch(e) {
-        return true;
+var tests = [
+  {
+    name: "Delete this in strict global code",
+    body: function () {
+         assert.isTrue(r, "We should have returned true from the global delete this above");
     }
-    return false;
-}
-
-function test4() {
-    "use strict";
-    let a = 'a';
-    try {
-        eval('delete a;');
-    } catch(e) {
-        return true;
+  },
+  {
+    name: "Delete this in strict mode nested function",
+    body: function () {
+        function test() {
+            "use strict";
+            return delete this;
+        }
+        
+        assert.isTrue(test(), "Delete this in strict nested function does nothing but returns true");
     }
-    return false;
-}
-
-function test5() {
-    "use strict";
-    try {
-        eval(`
-            function test5_eval() {
-                "use strict";
-                let a = 'a';
-                delete a;
+  },
+  {
+    name: "Delete new.target in strict mode nested function",
+    body: function () {
+        function test() {
+            "use strict";
+            return delete new.target;
+        }
+        
+        assert.isTrue(test(), "Delete new.target in strict nested function does nothing but returns true");
+    }
+  },
+  {
+    name: "Delete arguments in strict mode nested function",
+    body: function () {
+        function test() {
+            "use strict";
+            try {
+                eval('delete arguments;');
+            } catch(e) {
+                return true;
             }
-            test5_eval();
-        `);
-    } catch(e) {
-        return true;
+            return false;
+        }
+        
+        assert.isTrue(test(), "Delete arguments in strict nested function should throw early SyntaxError");
     }
-    return false;
-}
+  },
+  {
+    name: "Delete user identifier in strict mode nested function",
+    body: function () {
+        function test() {
+            "use strict";
+            let a = 'a';
+            try {
+                eval('delete a;');
+            } catch(e) {
+                return true;
+            }
+            return false;
+        }
+        
+        assert.isTrue(test(), "Delete user identifier in strict nested function should throw early SyntaxError");
+    }
+  },
+  {
+    name: "Delete user identifier in strict eval in strict mode nested function",
+    body: function () {
+        function test() {
+            "use strict";
+            try {
+                eval(`
+                    function test5_eval() {
+                        "use strict";
+                        let a = 'a';
+                        delete a;
+                    }
+                    test5_eval();
+                `);
+            } catch(e) {
+                return true;
+            }
+            return false;
+        }
+        
+        assert.isTrue(test(), "Delete user identifier in strict nested function should throw early SyntaxError");
+    }
+  },
+];
 
-r = r && test1() && test2() && test3() && test4() && test5();
-
-console.log(r ? 'PASS' : 'FAIL');
+testRunner.runTests(tests, { verbose: WScript.Arguments[0] != "summary" });
