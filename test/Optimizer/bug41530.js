7b59f023edebb676aa1c601e606d2c9817151cc7
Thomas Moore (CHAKRA) <thomasmo@microsoft.com>
2018-01-05 10:14:03 -0800

    Fix copyright build error

--- test/Optimizer/bug41530.js
+++ test/Optimizer/bug41530.js
@@ -1,3 +1,8 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
 function opt(arr) {
     if (arr.length <= 15)
         return;
