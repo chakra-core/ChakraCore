85af2a8ddc9d27c0c0522b98dc8f6086913bbe13
Taylor Woll <tawoll@ntdev.microsoft.com>
2018-01-08 11:19:42 -0800

    OS#15334058: Fix `class extends null` and `this` access for super property reference
    We were supporting some old version of the spec language around classes which extend from null. Instead of leaving `this` undeclared, we were constructing an object and assigning it the `this` binding. This meant we were not throwing ReferenceErrors on access to `this` when we should have been.
    
    Also fixed the case where a super property reference failed to access `this` resulting in trying to load a property from null.
    
    Fixes:
    https://microsoft.visualstudio.com/OS/_workitems/edit/15334058

--- lib/Runtime/Language/JavascriptOperators.cpp
+++ lib/Runtime/Language/JavascriptOperators.cpp
@@ -7093,11 +7093,6 @@ CommonNumber:
                     ctorProtoObj->EnsureProperty(Js::PropertyIds::constructor);
                     ctorProtoObj->SetEnumerable(Js::PropertyIds::constructor, FALSE);
 
-                    if (ScriptFunctionBase::Is(constructor))
-                    {
-                        ScriptFunctionBase::FromVar(constructor)->GetFunctionInfo()->SetBaseConstructorKind();
-                    }
-
                     break;
                 }
 
