54691067a4d2ff1fb6198124f0363e88d183a040
Michael Ferris <micfer@microsoft.com>
2018-01-11 17:31:50 -0800

    Regenerate Bytecode Headers

--- lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
+++ lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
@@ -4,6 +4,6 @@
 //-------------------------------------------------------------------------------------------------------
 // NOTE: If there is a merge conflict the correct fix is to make a new GUID.
 
-// {7126F68E-E40A-4E97-80D6-777755A28BDF}
+// {F717B63B-F844-4FFA-818B-E118FFC1179A}
 const GUID byteCodeCacheReleaseFileVersion =
-{ 0x7126F68E, 0xE40A, 0x4E97, { 0x80, 0xD6, 0x77, 0x77, 0x55, 0xA2, 0x8B, 0xDF } };
+{ 0xF717B63B, 0xF844, 0x4FFA, { 0x81, 0x8B, 0xE1, 0x18, 0xFF, 0xC1, 0x17, 0x9A } };
