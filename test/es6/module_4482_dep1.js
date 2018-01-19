8cc46eb471a8152b1a03aac7806667e3e9fbc35c
rhuanjl <rhuan.l@gmail.com>
2018-01-19 23:13:59 +0000

    Correct test case to be independent of module load order.

--- test/es6/module_4482_dep1.js
+++ test/es6/module_4482_dep1.js
@@ -13,11 +13,14 @@
 //v) Thing2 would not yet exist = segfault
 
 
-import Thing1 from './module_4482_dep2.js';
-import Thing2 from './module_4482_dep3.js';
+import Thing1 from 'module_4482_dep2.js';
+import Thing2 from 'module_4482_dep3.js';
 
 export { Thing1, Thing2 };
 
 export default
 function main()
-{}
+{
+	Thing1();
+	Thing2();
+}
