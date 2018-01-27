7f9822abb1430ae13186691798c279057773cec6
Taylor Woll <tawoll@ntdev.microsoft.com>
2018-01-26 18:10:09 -0800

    Fix MacOS build

--- lib/Runtime/Library/JavascriptRegExpConstructor.cpp
+++ lib/Runtime/Library/JavascriptRegExpConstructor.cpp
@@ -76,7 +76,7 @@ namespace Js
         {
             ScriptContext* scriptContext = this->GetScriptContext();
             const CharCount lastInputLen = lastInput->GetLength();
-            const wchar_t* lastInputStr = lastInput->GetString();
+            const char16* lastInputStr = lastInput->GetString();
             UnifiedRegex::RegexPattern* pattern = lastPattern;
 
             // When we perform a regex test operation it's possible the result of the operation will be loaded from a cache and the match will not be computed and updated in the ctor.
