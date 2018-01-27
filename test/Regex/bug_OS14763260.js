chakra-core/ChakraCore

commit 94491e811cfa237f68519b260337c038b6e187cb

address CR

lib/Runtime/Library/RegexHelper.cpp
@@ -686,8 +686,9 @@ namespace Js
         JavascriptRegExp::TraceTestCache(cacheHit, input, cachedInput, !useCache);
 #endif
 
-        if (useCache && cacheHit)
+        if (cacheHit)
         {
+            Assert(useCache);
             cachedResult = cache[cacheIndex].result;
             // for debug builds, let's still do the real test so we can validate values in the cache
 #if !DBG

commit 642331d00b997b835339d3d9505904c06b0831c0

OS#14763260: Correctly update RegExp.$1 after RegExp.prototype.test matches and used a cached value
Regression from https://github.com/Microsoft/ChakraCore/pull/3802 - if we hit the cache, we don't update the Regex constructor object with the last match and so retrieving properties about the match from the regex constructor object fails.

Fixed by adding an invalidation mechanism to the `JavascriptRegExpConstructor`. If we hit the cache, we'll mark the Regex constructor object last match properties as invalidated and we will then compute them on-demand the first time they're needed.

Fixes:
https://microsoft.visualstudio.com/web/wi.aspx?id=14763260

lib/Runtime/Library/JavascriptRegExpConstructor.cpp
@@ -17,6 +17,7 @@ namespace Js
     JavascriptRegExpConstructor::JavascriptRegExpConstructor(DynamicType * type) :
         RuntimeFunction(type, &JavascriptRegExp::EntryInfo::NewInstance),
         reset(false),
+        invalidatedLastMatch(false),
         lastPattern(nullptr),
         lastMatch() // undefined
     {
@@ -53,17 +54,44 @@ namespace Js
         this->lastInput = lastInput;
         this->lastMatch = lastMatch;
         this->reset = true;
+        this->invalidatedLastMatch = false;
+    }
+
+    void JavascriptRegExpConstructor::InvalidateLastMatch(UnifiedRegex::RegexPattern* lastPattern, JavascriptString* lastInput)
+    {
+        AssertMsg(lastPattern != nullptr, "lastPattern should not be null");
+        AssertMsg(lastInput != nullptr, "lastInput should not be null");
+        AssertMsg(JavascriptOperators::GetTypeId(lastInput) != TypeIds_Null, "lastInput should not be JavaScript null");
+
+        this->lastPattern = lastPattern;
+        this->lastInput = lastInput;
+        this->lastMatch.Reset();
+        this->reset = true;
+        this->invalidatedLastMatch = true;
     }
 
     void JavascriptRegExpConstructor::EnsureValues()
     {
         if (reset)
         {
-            Assert(!lastMatch.IsUndefined());
             ScriptContext* scriptContext = this->GetScriptContext();
-            UnifiedRegex::RegexPattern* pattern = lastPattern;
-            JavascriptString* emptyString = scriptContext->GetLibrary()->GetEmptyString();
             const CharCount lastInputLen = lastInput->GetLength();
+            const wchar_t* lastInputStr = lastInput->GetString();
+            UnifiedRegex::RegexPattern* pattern = lastPattern;
+
+            // When we perform a regex test operation it's possible the result of the operation will be loaded from a cache and the match will not be computed and updated in the ctor.
+            // In that case we invalidate the last match stored in the ctor and will need to compute it before it will be accessible via $1 etc.
+            // Since we only do this for the case of RegExp.prototype.test cache hit, we know several things:
+            //  - The regex is not global or sticky
+            //  - There was a match (test returned true)
+            if (invalidatedLastMatch)
+            {
+                this->lastMatch = RegexHelper::SimpleMatch(scriptContext, pattern, lastInputStr, lastInputLen, 0);
+                invalidatedLastMatch = false;
+            }
+
+            Assert(!lastMatch.IsUndefined());
+            JavascriptString* emptyString = scriptContext->GetLibrary()->GetEmptyString();
             // IE8 quirk: match of length 0 is regarded as length 1
             CharCount lastIndexVal = lastMatch.EndOffset();
             this->index = JavascriptNumber::ToVar(lastMatch.offset, scriptContext);
@@ -82,7 +110,7 @@ namespace Js
                 // every match is prohibitively slow. Instead, run the match again using the known last input string.
                 if (!pattern->WasLastMatchSuccessful())
                 {
-                    RegexHelper::SimpleMatch(scriptContext, pattern, lastInput->GetString(), lastInputLen, lastMatch.offset);
+                    RegexHelper::SimpleMatch(scriptContext, pattern, lastInputStr, lastInputLen, lastMatch.offset);
                 }
                 Assert(pattern->WasLastMatchSuccessful());
                 for (int groupId = 1; groupId < min(numGroups, NumCtorCaptures); groupId++)

lib/Runtime/Library/JavascriptRegExpConstructor.h
@@ -53,11 +53,13 @@ namespace Js
         bool GetPropertyBuiltIns(PropertyId propertyId, Var* value, BOOL* result);
         bool SetPropertyBuiltIns(PropertyId propertyId, Var value, BOOL* result);
         void SetLastMatch(UnifiedRegex::RegexPattern* lastPattern, JavascriptString* lastInput, UnifiedRegex::GroupInfo lastMatch);
+        void InvalidateLastMatch(UnifiedRegex::RegexPattern* lastPattern, JavascriptString* lastInput);
 
         void EnsureValues();
 
         Field(UnifiedRegex::RegexPattern*) lastPattern;
         Field(JavascriptString*) lastInput;
+        Field(bool) invalidatedLastMatch; // true if last match must be recalculated before use
         Field(UnifiedRegex::GroupInfo) lastMatch;
         Field(bool) reset; // true if following fields must be recalculated from above before first use
         Field(Var) lastParen;

lib/Runtime/Library/RegexHelper.cpp
@@ -691,12 +691,19 @@ namespace Js
         {
             Assert(useCache);
             cachedResult = (cache->resultBV.Test(cacheIndex) != 0);
+
+            // If our cache says this test should produce a match (which we aren't going to compute),
+            // notify the Ctor to invalidate the last match so it must be recomputed before access.
+            if (cachedResult)
+            {
+                InvalidateLastMatchOnCtor(scriptContext, regularExpression, input);
+            }
+
             // for debug builds, let's still do the real test so we can validate values in the cache
 #if !DBG
             return JavascriptBoolean::ToVar(cachedResult, scriptContext);
 #endif
         }
-
 
         CharCount offset;
         if (!GetInitialOffset(isGlobal, isSticky, regularExpression, inputLength, offset))
@@ -2085,6 +2092,16 @@ namespace Js
                 : regularExpression->GetPattern();
             scriptContext->GetLibrary()->GetRegExpConstructor()->SetLastMatch(pattern, lastInput, lastSuccessfulMatch);
         }
+    }
+
+    void RegexHelper::InvalidateLastMatchOnCtor(ScriptContext* scriptContext, JavascriptRegExp* regularExpression, JavascriptString* lastInput, bool useSplitPattern)
+    {
+        Assert(lastInput);
+
+        UnifiedRegex::RegexPattern* pattern = useSplitPattern
+            ? regularExpression->GetSplitPattern()
+            : regularExpression->GetPattern();
+        scriptContext->GetLibrary()->GetRegExpConstructor()->InvalidateLastMatch(pattern, lastInput);
     }
 
     bool RegexHelper::GetInitialOffset(bool isGlobal, bool isSticky, JavascriptRegExp* regularExpression, CharCount inputLength, CharCount& offset)

lib/Runtime/Library/RegexHelper.h
@@ -58,6 +58,8 @@ namespace Js
             , UnifiedRegex::GroupInfo lastSuccessfulMatch
             , bool useSplitPattern );
 
+        static void InvalidateLastMatchOnCtor(ScriptContext* scriptContext, JavascriptRegExp* regularExpression, JavascriptString* lastInput, bool useSplitPattern = false);
+
         static bool GetInitialOffset(bool isGlobal, bool isSticky, JavascriptRegExp* regularExpression, CharCount inputLength, CharCount& offset);
         static JavascriptArray* CreateMatchResult(void *const stackAllocationPointer, ScriptContext* scriptContext, bool isGlobal, int numGroups, JavascriptString* input);
         static void FinalizeMatchResult(ScriptContext* scriptContext, bool isGlobal, JavascriptArray* arr, UnifiedRegex::GroupInfo match);

test/Regex/bug_OS14763260.js
@@ -1,0 +1,26 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
+WScript.LoadScriptFile("..\\UnitTestFramework\\UnitTestFramework.js");
+
+var tests = [
+  {
+    name: "Verify last match invalidated as expected",
+    body: function () {
+        const r1 = /(abc)/;
+        const r2 = /(def)/;
+        const s1 = "abc";
+        const s2 = "def";
+         
+        r1.test(s1);
+        r2.test(s2);
+        r1.test(s1);
+
+        assert.areEqual("abc", RegExp.$1, "Stale last match should be invalidated by second r1.test(s1)");
+    }
+  },
+];
+
+testRunner.runTests(tests, { verbose: WScript.Arguments[0] != "summary" });

test/Regex/rlexe.xml
@@ -206,4 +206,10 @@
       <compile-flags>-args summary -endargs</compile-flags>
     </default>
   </test>
+  <test>
+    <default>
+      <files>bug_OS14763260.js</files>
+      <compile-flags>-args summary -endargs</compile-flags>
+    </default>
+  </test>
 </regress-exe>