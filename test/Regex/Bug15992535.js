2f73290d6b86cbe87bc0bdbf521fe6ead4d268ae
Richard Cobbe <ricobbe@microsoft.com>
2018-02-22 12:33:52 -0800

    Minimize unit test; add "slow" tag since it still takes 10sec on my desktop

--- test/Regex/Bug15992535.js
+++ test/Regex/Bug15992535.js
@@ -7,25 +7,18 @@
 // intended behavior is that the regex parser hits an AssertOrFailFast; we may revisit
 // that error behavior in the future.
 
-try { function f() {
-    (((-554663171.9).toTimeString));
-}
-
-let call = new Proxy((forEach.call(has , (call-- ))), {});  // proxy calls set the flag
-call.call(f); } catch(err) { }
-
 try { function i_want_to_break_free() {
-    var n = 0x40000;
-    var m = 10;
-    var regex = new RegExp("(ab)".repeat(n), "g"); // g flag to trigger the vulnerable path
+    var n = 0x8000;
+    var m = 2;
+    var regex = new RegExp("(ab)".repeat(n), "g");
     var part = "ab".repeat(n); // matches have to be at least size 2 to prevent interning
     var s = (part + "|").repeat(m);
     while (true) {
         var cnt = 0;
         var ary = (function () {
             Object.toUpperCase(asinh, (() => {
-        let arguments;
-    }));
+                let arguments;
+            }));
             decodeURIComponent.splice(0);        // head seg is now length=0
             trim.length = 100000;  // increase the length of the array to 100000 so to access head segment
             return 42;
@@ -44,35 +37,5 @@ try { function i_want_to_break_free() {
 }
 try { i_want_to_break_free(); } catch (e) { }
 ((search.valueOf)); } catch(err) { }
-
-try { function opt(obj) {
-for (let i in obj.inlinee.call({})) {
-}
-
-for (let i in obj.inlinee.call((exec.__proto__))) {
-}
-}
-
-function main() {
-let obj = {
-inlinee: ((setUTCSeconds.call(toLocaleLowerCase , log1p, ((function () {
-          (isNaN.ReferenceError('ary[6] = ' + (UInt32Array[6]|0)));
-          return 3;
-        })), (toUpperCase.push((({valueOf : atanh}).getFloat64-- ), Math.ceil(opt.setUTCMilliseconds), Object.create(getMinutes, (reduce.byteLength)), (Uint8ClampedArray.length / (trunc.length == 0 ? 1 : (function() {
-        unescape = [99,99,99];
-
-        return subarray;
-    }))), ('µi'+'!Z'+'*%' + 'r!' != '!'), (true * (getUint32 - -704503840)), (((function () {;}) instanceof ((({}) ) ? Boolean : Object))), displayName, (typeof(abs)  == 'object') , ((inlinee ? charAt = round : 1)), parseInt("10GBM4CB92UEO", 35), ('µi'+'!Z'+'*%' + 'r!' != '!'), Object.create({lastIndexOf: -8.89468121267151E+18, size: map.length, encodeURIComponent: apply, valueOf: (getInt32 ^ 0xc761c23c)})))
-    , setInt32[(13)])))
-};
-
-for (let i = 0; i < 10000; i++)
-opt(obj);
-}
-
-main(); } catch(err) { }
-
-try { this.__defineGetter__("x", (a = (function f() { return; (function() {}); })()) => { });
-x; } catch(err) { }
 
 print("Didn't get expected fail fast");
