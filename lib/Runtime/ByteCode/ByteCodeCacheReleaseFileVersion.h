12b22d8a5abb5234265daf60bf97fa008de231f5
Jack Horton <jahorto@microsoft.com>
2018-03-26 15:11:55 -0700

    Update bytecode version, add script to make doing so easier

--- lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
+++ lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
@@ -3,7 +3,8 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 // NOTE: If there is a merge conflict the correct fix is to make a new GUID.
+// This file was generated with core\tools\update_bytecode_version.ps1
 
-// {790F966D-646F-4CBF-86C2-6C8FECA645EA}
+// 6a68e4f0-e0ba-4332-b23b-9f509144497a
 const GUID byteCodeCacheReleaseFileVersion =
-{ 0x790F966D, 0x646F, 0x4CBF, { 0x86, 0xC2, 0x6C, 0x8F, 0xEC, 0xA6, 0x45, 0xEA } };
+{ 0x6a68e4f0, 0xe0ba, 0x4332, { 0xb2, 0x3b, 0x9f, 0x50, 0x91, 0x44, 0x49, 0x7a } };
---
+++ tools/update_bytecode_version.ps1
@@ -0,0 +1,39 @@
+# -------------------------------------------------------------------------------------------------------
+# Copyright (C) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+# -------------------------------------------------------------------------------------------------------
+
+# Generates a new ByteCodeCacheReleaseFileVersion.h
+$scriptRoot=Split-Path -Path $MyInvocation.MyCommand.Path
+$versionHeader="$scriptRoot\..\lib\Runtime\ByteCode\ByteCodeCacheReleaseFileVersion.h"
+
+Write-Host "Writing file to $versionHeader"
+Remove-Item -Path $versionHeader -Force
+
+function Write-Header() {
+    Write-Output $args | Out-File -Encoding ASCII -Append $versionHeader
+}
+
+$copyright=@"
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+// NOTE: If there is a merge conflict the correct fix is to make a new GUID.
+// This file was generated with core\tools\update_bytecode_version.ps1
+
+"@
+
+$version=[Guid]::NewGuid().ToString()
+
+Write-Header $copyright
+Write-Header "// $version"
+Write-Header "const GUID byteCodeCacheReleaseFileVersion ="
+
+$version -match "^(\w{8})-(\w{4})-(\w{4})-(\w{4}-\w{12})$" | Out-Null
+$majorParts=$Matches
+$Matches[4] -match "^(\w{2})(\w{2})-(\w{2})(\w{2})(\w{2})(\w{2})(\w{2})(\w{2})$" | Out-Null
+$minorParts=$Matches
+$minorStr="0x{0}, 0x{1}, 0x{2}, 0x{3}, 0x{4}, 0x{5}, 0x{6}, 0x{7}" -f $minorParts[1], $minorParts[2], $minorParts[3], $minorParts[4], $minorParts[5], $minorParts[6], $minorParts[7], $minorParts[8]
+$majorStr="0x{0}, 0x{1}, 0x{2}" -f $majorParts[1], $majorParts[2], $majorParts[3]
+Write-Header "{ $majorStr, { $minorStr } };"
