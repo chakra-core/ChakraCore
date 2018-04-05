32c4038911cd4bcdb077fa16f28d20fa191780c4
Richard Cobbe <ricobbe@microsoft.com>
2018-04-05 10:45:49 -0700

    Check for OAuth token in pre_build.ps1 before attempting to fetch ChakraHub notes

--- Build/scripts/pre_build.ps1
+++ Build/scripts/pre_build.ps1
@@ -175,19 +175,24 @@ $CommitMessage
     ## Since we have to handle the case where a full build doesn't have a ChakraHub note anyway, just
     ## add the logic to the core script.
 
-    git -c http.extraheader="AUTHORIZATION: bearer ${Env:System.AccessToken}" fetch origin refs/notes/ChakraHub:refs/notes/ChakraHub
-    Write-Output "looking for notes"
-    ## One might be tempted to redirect the output of the "git notes" command on the next line to $null.  Don't do that;
-    ## it causes the check to fail even when git notes are present.  However, we do have to redirect stderr, because
-    ## git prints something to stderr if it doesn't find a note, and that would cause VSTS to think that the build failed.
-    if (git notes --ref=ChakraHub list HEAD 2>&1)
+    ## Without the OAuth token, the git fetch command hangs, causing the entire build to time out.
+    if (${Env:System.AccessToken})
     {
-        $testConfig = (git notes --ref=ChakraHub show HEAD) -join "`n" | ConvertFrom-Json
-        $changeJson | Add-Member -type NoteProperty -name TestConfig -value $testConfig
-    }
-    else
-    {
-        Write-Output "no git notes found"
+        git -c http.extraheader="AUTHORIZATION: bearer ${Env:System.AccessToken}" fetch origin refs/notes/ChakraHub:refs/notes/ChakraHub
+        Write-Output "looking for notes"
+        ## One might be tempted to redirect the output of the "git notes" command on the next line to $null.  Don't do that;
+        ## it causes the check to fail even when git notes are present.  However, we do have to redirect stderr, because
+        ## git prints something to stderr if it doesn't find a note, and that would cause VSTS to think that the build failed.
+        if (git notes --ref=ChakraHub list HEAD 2>&1)
+        {
+            Write-Output "found git notes; adding to change.json"
+            $testConfig = (git notes --ref=ChakraHub show HEAD) -join "`n" | ConvertFrom-Json
+            $changeJson | Add-Member -type NoteProperty -name TestConfig -value $testConfig
+        }
+        else
+        {
+            Write-Output "no git notes found"
+        }
     }
 
     Write-Output "-----"
