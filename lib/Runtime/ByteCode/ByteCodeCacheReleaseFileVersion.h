56df095ef6d73813219066b6f2be48e867c6bb2a
Jack Horton (CHAKRA) <jahorto@microsoft.com>
2018-05-07 12:10:19 -0700

    Update bytecode version

--- lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
+++ lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
@@ -3,7 +3,8 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 // NOTE: If there is a merge conflict the correct fix is to make a new GUID.
+// This file was generated with core\tools\update_bytecode_version.ps1
 
-// {752B5975-74CF-499D-8438-F7028EB3CAC4}
+// 15f08b09-6802-4d9a-80b2-86ce974c1fec
 const GUID byteCodeCacheReleaseFileVersion =
-{ 0x752B5975, 0x74CF, 0x499D, { 0x84, 0x38, 0xF7, 0x02, 0x8E, 0xB3, 0xCA, 0xC4 } };
+{ 0x15f08b09, 0x6802, 0x4d9a, { 0x80, 0xb2, 0x86, 0xce, 0x97, 0x4c, 0x1f, 0xec } };
