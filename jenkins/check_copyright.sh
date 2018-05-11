81c63d20f8f3f6c56662f0b8fa379e454a52b734
Jimmy Thomson <jithomso@microsoft.com>
2018-05-11 10:01:49 -0700

    Adding collateral for signing nuget packages

--- jenkins/check_copyright.sh
+++ jenkins/check_copyright.sh
@@ -37,6 +37,8 @@ git diff --name-only `git merge-base origin/$ghprbTargetBranch HEAD` HEAD |
     grep -v -E '\.cmake$' |
     grep -v -E '\.json$' |
     grep -v -E '\.man$' |
+    grep -v -E '\.proj$' |
+    grep -v -E 'packages.config$' |
     grep -v -E 'lib/wabt/.*' |
     grep -v -E 'test/WasmSpec/.*$' |
     grep -v -E 'test/UnitTestFramework/yargs.js$' |
