8e1ec0a029a87143e1acfe8242fe1e945dfad5e5
Thomas Moore (CHAKRA) <thomasmo@microsoft.com>
2018-06-08 09:24:32 -0700

    Fix assertion when dumping encoder with foreground JIT
    While outputting offsets in the EncoderPhase, several asserts can be hit because the state of the EntryPointInfo is Queued, where it is expected to be Recorded or Done. The effect is that there is no base address available for the right output.
    The fix is to move the calls with that dependency to after the state transitions to Recorded.

--- lib/Backend/NativeCodeGenerator.cpp
+++ lib/Backend/NativeCodeGenerator.cpp
@@ -1192,6 +1192,13 @@ NativeCodeGenerator::CodeGen(PageAllocator * pageAllocator, CodeGenWorkItem* wor
 
     workItem->GetEntryPoint()->SetCodeGenRecorded((Js::JavascriptMethod)jitWriteData.thunkAddress, (Js::JavascriptMethod)jitWriteData.codeAddress, jitWriteData.codeSize, (void *)this);
 
+    if (PHASE_DUMP(Js::EncoderPhase, workItem->GetFunctionBody()) && Js::Configuration::Global.flags.Verbose && !JITManager::GetJITManager()->IsOOPJITEnabled())
+    {
+        workItem->GetEntryPoint()->DumpNativeOffsetMaps();
+        workItem->GetEntryPoint()->DumpNativeThrowSpanSequence();
+        Output::Flush();
+    }
+
     if (jitWriteData.hasBailoutInstr != FALSE)
     {
         body->SetHasBailoutInstrInJittedCode(true);
