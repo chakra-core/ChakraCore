a157300805caa44bfc169010ae0134d22f55ec31
Derek Morris <demorri@microsoft.com>
2018-06-28 15:09:39 -0700

    Disable one more warning in catch.hpp

--- bin/NativeTests/JsRTApiTest.cpp
+++ bin/NativeTests/JsRTApiTest.cpp
@@ -3,6 +3,7 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 #include "stdafx.h"
+#pragma warning(disable:26434)
 #pragma warning(disable:26439)
 #pragma warning(disable:26451)
 #pragma warning(disable:26495)
