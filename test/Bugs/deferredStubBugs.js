2f880434b81afcc65161fd8a3c24edc68b5ed685
Taylor Woll <tawoll@ntdev.microsoft.com>
2018-07-27 13:54:27 -0700

    Add unit test coverage

--- test/Bugs/deferredStubBugs.js
+++ test/Bugs/deferredStubBugs.js
@@ -4,6 +4,7 @@
 //-------------------------------------------------------------------------------------------------------
 
 let pass = 'pass';
+let fail = 'fail';
 
 function func4(a = 123) {
     function v8() {
@@ -56,3 +57,30 @@ var func11 = (a, b = () => { return pass; }, c) => {
     return func12();
 }
 console.log(func11()()());
+
+function func13(a = (b = () => pass, c = () => fail) => b()) {
+    return a();
+}
+console.log(func13());
+
+function func14(a = (b = () => { return fail; }, c = () => { return pass; }) => { return c(); }) {
+    return a();
+}
+console.log(func14());
+
+function func15(a = class A { meth() { return pass } static meth2() { return fail; } }, b = v => fail, c = (v) => { return fail }, d = fail) {
+    return new a().meth();
+}
+console.log(func15());
+function func16(a = class A { meth() { return fail } static meth2() { return pass; } }, b = v => fail, c = (v) => { return fail }, d = fail) {
+    return a.meth2();
+}
+console.log(func16());
+function func17(a = class A { meth() { return fail } static meth2() { return fail; } }, b = v => pass, c = (v) => { return fail }, d = fail) {
+    return b();
+}
+console.log(func17());
+function func18(a = class A { meth() { return fail } static meth2() { return fail; } }, b = v => fail, c = (v) => { return pass }, d = fail) {
+    return c();
+}
+console.log(func18());
