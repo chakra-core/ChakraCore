80bb9c584a7946ab81f462440301570112ef1bc2
Seth Brenith <sethb@microsoft.com>
2018-08-13 10:46:43 -0700

    Update global toStringTag to match attributes from v8

--- test/Object/toStringWithGlobalObject.js
+++ test/Object/toStringWithGlobalObject.js
@@ -6,3 +6,7 @@
 let global = new Function('return this;')();
 console.log(global.toString());
 console.log(global[Symbol.toStringTag]);
+let descriptor = Object.getOwnPropertyDescriptor(global, Symbol.toStringTag);
+console.log(descriptor.writable);
+console.log(descriptor.enumerable);
+console.log(descriptor.configurable);
