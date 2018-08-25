ec7a0672e215719f9617e61fa29d03110ca87889
Doug Ilijev <doilij@microsoft.com>
2018-08-24 19:20:21 -0700

    Update release/1.11 to version 1.11.0, update GUID, and set PRERELEASE

--- lib/Common/ChakraCoreVersion.h
+++ lib/Common/ChakraCoreVersion.h
@@ -16,8 +16,8 @@
 
 // ChakraCore version number definitions (used in ChakraCore binary metadata)
 #define CHAKRA_CORE_MAJOR_VERSION 1
-#define CHAKRA_CORE_MINOR_VERSION 10
-#define CHAKRA_CORE_PATCH_VERSION 2
+#define CHAKRA_CORE_MINOR_VERSION 11
+#define CHAKRA_CORE_PATCH_VERSION 0
 #define CHAKRA_CORE_VERSION_RELEASE_QFE 0 // Redundant with PATCH_VERSION. Keep this value set to 0.
 
 // -------------
@@ -55,7 +55,7 @@
 
 // ChakraCore RELEASE and PRERELEASE flags
 #define CHAKRA_CORE_VERSION_RELEASE 1
-#define CHAKRA_CORE_VERSION_PRERELEASE 0
+#define CHAKRA_CORE_VERSION_PRERELEASE 1
 
 // Chakra RELEASE flag
 // Mostly redundant with CHAKRA_CORE_VERSION_RELEASE,
