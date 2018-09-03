c279dfdb2f05f1fa1837e25435112f1306a8a205
Richard <rhuan.l@gmail.com>
2018-09-03 20:53:32 +0100

    bytecode regen

--- lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
+++ lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
@@ -5,6 +5,6 @@
 // NOTE: If there is a merge conflict the correct fix is to make a new GUID.
 // This file was generated with tools\update_bytecode_version.ps1
 
-// {6CC5964E-CBF2-4053-99DA-987794182D7E}
+// {BAC3A947-4873-4DAF-AC60-915116FFE744}
 const GUID byteCodeCacheReleaseFileVersion =
-{ 0x6CC5964E, 0xCBF2, 0x4053, { 0x99, 0xDA, 0x98, 0x77, 0x94, 0x18, 0x2D, 0x7E } };
+{ 0xBAC3A947, 0x4873, 0x4DAF, { 0xAC, 0x60, 0x91, 0x51, 0x16, 0xFF, 0xE7, 0x44 } };
