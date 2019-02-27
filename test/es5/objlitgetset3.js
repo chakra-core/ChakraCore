a653bc9024d45688777d75f5395bf69034446641
Paul Leathers <pleath@microsoft.com>
2019-02-27 10:13:56 -0800

    Fix assert when building newly rooted path type

---
+++ test/es5/objlitgetset3.js
@@ -0,0 +1,12 @@
+var v_8184 = {
+    d: "vEH1gs5LChZC6hiytm4BhQY6D1BvBbSR",
+    set d(x) { this.d = x + 0x1337; },
+    f: undefined,
+    d: -0,
+    m: 794.4080805517691,
+    get a() { return 1337; }
+};
+
+v_8184.__proto__ = {};
+if (v_8184.d === -0)
+    WScript.Echo('pass');
