1c5ab56e1d639e96a1bda43a2430eee39c16a262
Akrosh Gandhi <akroshg@microsoft.com>
2019-03-19 16:20:59 -0700

    updating bytecodecache guid

--- lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
+++ lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
@@ -4,6 +4,6 @@
 //-------------------------------------------------------------------------------------------------------
 // NOTE: If there is a merge conflict the correct fix is to make a new GUID.
 
-// {11BA1CDD-86FC-4489-95EB-355A78E78EF8}
+// {3EDF890E-55F9-4646-A9AC-53127700D405}
 const GUID byteCodeCacheReleaseFileVersion =
-{ 0x11ba1cdd, 0x86fc, 0x4489, { 0x95, 0xeb, 0x35, 0x5a, 0x78, 0xe7, 0x8e, 0xf8 } };
+{ 0x3EDF890E, 0x55F9, 0x4646,{ 0xA9, 0xAC, 0x53, 0x12, 0x77, 0x00, 0xD4, 0x05 } };
