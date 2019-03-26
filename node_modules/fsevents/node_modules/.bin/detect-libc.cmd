c8b6bc9f8f7be9a55bc9438412c18a2652d7403d
Edwin Perez <edwinfrc@github.com>
2019-03-25 19:39:14 -0600

    commit3

--- node_modules/fsevents/node_modules/.bin/detect-libc.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\detect-libc\bin\detect-libc.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\detect-libc\bin\detect-libc.js" %*
-)
--- node_modules/fsevents/node_modules/.bin/needle.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\needle\bin\needle" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\needle\bin\needle" %*
-)
