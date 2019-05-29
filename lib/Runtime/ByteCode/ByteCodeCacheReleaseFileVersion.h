9be99cd461fbd255d64f502dc2e0d6e63b1aa757
Taylor Woll <tawoll@ntdev.microsoft.com>
2019-05-29 16:39:33 -0700

    Update bytecode release version

--- lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
+++ lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
@@ -4,6 +4,6 @@
 //-------------------------------------------------------------------------------------------------------
 // NOTE: If there is a merge conflict the correct fix is to make a new GUID.
 
-// {2734B2C7-9500-45AF-AAA6-FABB7B9C3F81}
+// {5EEF3B18-B808-42EF-BA86-A68AA3DBEEBA}
 const GUID byteCodeCacheReleaseFileVersion =
-{ 0x2734b2c7, 0x9500, 0x45af, { 0xaa, 0xa6, 0xfa, 0xbb, 0x7b, 0x9c, 0x3f, 0x81 } };
+{ 0x5eef3b18, 0xb808, 0x42ef, { 0xba, 0x86, 0xa6, 0x8a, 0xa3, 0xdb, 0xee, 0xba } };
