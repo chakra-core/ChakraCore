242c59ea40c0428e1ced7366bf2c28bfbdda1999
Taylor Woll <tawoll@ntdev.microsoft.com>
2019-07-02 13:12:05 -0700

    [CVE-2019-1131] Chakra Type confusion

--- lib/Parser/Parse.cpp
+++ lib/Parser/Parse.cpp
@@ -4664,10 +4664,7 @@ ParseNodePtr Parser::ParseMemberList(LPCOLESTR pNameHint, uint32* pNameHintLengt
                     }
                 }
 
-                if (buildAST)
-                {
-                    CheckArgumentsUse(pidHint, GetCurrentFunctionNode());
-                }
+                CheckArgumentsUse(pidHint, GetCurrentFunctionNode());
 
                 bool couldBeObjectPattern = !isObjectPattern && m_token.tk == tkAsg;
                 // Saving the current state as we may change the isObjectPattern down below.
