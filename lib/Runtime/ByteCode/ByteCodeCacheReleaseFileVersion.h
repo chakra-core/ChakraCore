b75847eb1ef4136d49e119c072266d8874a6e41e
Taylor Woll <tawoll@ntdev.microsoft.com>
2019-07-19 20:27:31 -0700

    [CVE-2019-1217]

--- lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
+++ lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
@@ -5,6 +5,6 @@
 // NOTE: If there is a merge conflict the correct fix is to make a new GUID.
 // This file was generated with tools\update_bytecode_version.ps1
 
-// {3096A219-129D-4A4A-A61C-186D03BB25B7}
+// {81AEEA4B-AE4E-40C0-848F-6DB7C5F49F55}
 const GUID byteCodeCacheReleaseFileVersion =
-{ 0x3096a219, 0x129d, 0x4a4a, { 0xa6, 0x1c, 0x18, 0x6d, 0x3, 0xbb, 0x25, 0xb7 } };
+{ 0x81AEEA4B, 0xAE4E, 0x40C0, { 0x84, 0x8F, 0x6D, 0xB7, 0xC5, 0xF4, 0x9F, 0x55 } };
