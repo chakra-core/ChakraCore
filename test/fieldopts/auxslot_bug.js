49edc91b6ac535bc0b2495b54d2e9b067fbfab95
Louis Lafreniere <louisl@microsoft.com>
2019-07-25 16:46:26 -0700

    CheckFixedFld invariants and CSE issues
    Hoisting and CSE'ing CheckFixedFlds causes issues for objTypeSpec, as it can invalidate dependencies on the availability of the type sym or aux slot sym.

---
+++ test/fieldopts/auxslot_bug.js
@@ -0,0 +1,27 @@
+var obj0 = {};
+var obj1 = {};
+var arrObj0 = {};
+var func1 = function () {
+  function func5() {
+  }
+  obj6 = func5();
+  while (obj1) {
+    obj6 = obj6;
+    if (ary.shift() >> new EvalError()) {
+      length;
+    } else {
+      break;
+    }
+  }
+};
+var func3 = function () {
+  func1();
+};
+obj0.method0 = func3;
+obj0.method1 = obj0.method0;
+arrObj0.method0 = obj0.method1;
+var ary = Array();
+
+for (var i = 0; i < 200; i++) arrObj0.method0();
+
+print("Passed");
