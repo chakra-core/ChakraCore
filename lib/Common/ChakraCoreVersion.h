5989c6e038d80f92dcd8e10d725cdf45396201bb
Paul Leathers <pleath@microsoft.com>
2019-10-08 11:10:05 -0700

    Update version to 1.11.14

--- lib/Common/ChakraCoreVersion.h
+++ lib/Common/ChakraCoreVersion.h
@@ -17,7 +17,7 @@
 // ChakraCore version number definitions (used in ChakraCore binary metadata)
 #define CHAKRA_CORE_MAJOR_VERSION 1
 #define CHAKRA_CORE_MINOR_VERSION 11
-#define CHAKRA_CORE_PATCH_VERSION 13
+#define CHAKRA_CORE_PATCH_VERSION 14
 #define CHAKRA_CORE_VERSION_RELEASE_QFE 0 // Redundant with PATCH_VERSION. Keep this value set to 0.
 
 // -------------
