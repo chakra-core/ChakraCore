a0d4d4921ab36fb9340c66e22b01a5684466ac77
rhuanjl <rhuan.l@gmail.com>
2021-02-04 00:58:21 +0000

    Skip Array push fast path if prototype unsafe - push fast path doesn't walk prototype chain - put a check to avoid fast path if proto has getters/setters etc - minor re-factor to avoid doing checks and type coercians multiple times - add test case - tidy up push3.js test case and remove its unnecessary baseline file

--- test/Array/push3.js
+++ test/Array/push3.js
@@ -1,75 +1,75 @@
 //-------------------------------------------------------------------------------------------------------
 // Copyright (C) Microsoft. All rights reserved.
+// Copyright (c) 2021 ChakraCore Project Contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
+
+function confirmThrows(call, errorType)
+{
+  var success = false;
+  try {
+    call();
+  } catch (e) {
+    if (e instanceof errorType) {
+      success = true;
+    }
+  }
+  if (!success) {
+    throw new Error ("Test was meant to throw " + errorType + "but it didn't")
+  }
+}
 
 function test0() {
     var GiantPrintArray = [];
     var obj1 = {};
     function v9870() {
-    try {
-        var arr = [];
-        var v9872 = [];
-        Object.defineProperty(Array.prototype, "0", { configurable: true, get: function () { return 30; } });
-        GiantPrintArray.push(v9872.indexOf(30));
-      }
-    catch(ex) {
-        WScript.Echo(ex.message);
-      }
+      var arr = [];
+      var v9872 = [];
+      Object.defineProperty(Array.prototype, "0", { configurable: true, get: function () { return 30; } });
+      GiantPrintArray.push(v9872.indexOf(30));
     }
-    v9870();
-    WScript.Echo(GiantPrintArray[0]);
+    confirmThrows(v9870, TypeError);
+    if (GiantPrintArray[0] !== 30) {
+      throw new Error('Wrong value set');
+    }
 };
 test0();
 test0();
 
 function test1() {
-    try {
-        var arr = [];
-        Object.preventExtensions(arr);
-        arr.push(0);
-    }
-    catch(ex) {
-        WScript.Echo(ex.message);
-    }
+  var arr = [];
+  Object.preventExtensions(arr);
+  arr.push(0);
 }
-test1();
-test1();
+confirmThrows(test1, TypeError);
+confirmThrows(test1, TypeError);
 
-    Object.defineProperty(Object.prototype,"a",{get:function(){return 8 }});
+Object.defineProperty(Object.prototype,"a",{get:function(){return 8 }});
+
 function test2() {
-var GiantPrintArray = [];
+  var GiantPrintArray = [];
   var obj1 = {};
   var func1 = function(){
-    try {
-      GiantPrintArray.push(obj1.a);
-    }
-    catch(ex) {
-        WScript.Echo(ex.message);
-    }
+    GiantPrintArray.push(obj1.a);
   }
-  func1();
+
+  confirmThrows(func1, TypeError);
 
   function v31079()
   {
     Object.defineProperty(Array.prototype, "4", {configurable : true, get: function(){return 15;}});
-    try {
-      GiantPrintArray.push(1);
-      GiantPrintArray.push(1);
-    }
-    catch(ex) {
-        WScript.Echo(ex.message);
-    }
-
+    GiantPrintArray.push(1);
+    GiantPrintArray.push(1);
   }
 
-  v31079();
-  v31079();
+  confirmThrows(v31079, TypeError);
+  confirmThrows(v31079, TypeError);
 
-  for(var i =0;i<GiantPrintArray.length;i++){
- WScript.Echo(GiantPrintArray[i]);
- };
+  if (GiantPrintArray.length > 0) {
+    throw new Error ("Length should be 0");
+  }
 }
 test2();
 test2();
 
+print("pass");
