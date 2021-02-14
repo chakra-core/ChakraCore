bcd7973e600aa0fa965df548dd309c873a93542e
MadProbe <49519179+MadProbe@users.noreply.github.com>
2021-02-14 09:30:15 +0300

    fix space

--- lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
+++ lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
@@ -3,10 +3,9 @@
 // Copyright (c) 2021 ChakraCore Project Contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
-        // NOTE: If there is a merge conflict the correct fix is to make a new GUID.
-        // This file was generated with tools/regenByteCode.py
+// NOTE: If there is a merge conflict the correct fix is to make a new GUID.
+// This file was generated with tools/regenByteCode.py
 
-        // {4f97d737-2b2b-49a9-8a25-aa476fafb4c0}
-        const GUID byteCodeCacheReleaseFileVersion =
-        { 0x4f97d737, 0x2b2b, 0x49a9, {0x8a, 0x25, 0xaa, 0x47, 0x6f, 0xaf, 0xb4, 0xc0 } };
-        
+// {4f97d737-2b2b-49a9-8a25-aa476fafb4c0}
+const GUID byteCodeCacheReleaseFileVersion =
+{ 0x4f97d737, 0x2b2b, 0x49a9, {0x8a, 0x25, 0xaa, 0x47, 0x6f, 0xaf, 0xb4, 0xc0 } };
