738a80e1b38a54d561b8b9a208b419ea21f0e66a
MadProbe <49519179+MadProbe@users.noreply.github.com>
2021-03-14 12:29:26 +0300

    Improve the tests

--- test/Error/error_cause.js
+++ test/Error/error_cause.js
@@ -7,58 +7,84 @@
 WScript.LoadScriptFile("..\\UnitTestFramework\\UnitTestFramework.js");
 
 
-const o = {};
+function makeTestsFor(ErrorConstructor) {
+    const name = ErrorConstructor.name;
+    const message = "Test message";
+    const o = {};
+    return [
+        {
+            name: `"cause" in ${ name }.prototype.cause === false`,
+            body: function () {
+                assert.isFalse("cause" in ErrorConstructor.prototype, `Cause property must not exist in ${ name }.prototype`);
+            }
+        },
+        {
+            name: `"cause" in ${ name }().cause === false`,
+            body: function () {
+                assert.isFalse("cause" in ErrorConstructor(), `${ name }().cause should not be defined if not specified by cause property of options parameter in ${ name } constructor`);
+            }
+        },
+        {
+            name: `message property is defined when ${ name } called with one argument, not cause`,
+            body: function () {
+                assert.isTrue("message" in ErrorConstructor(message), `message property is defined when ${ name } called with one argument, not cause`);
+                assert.isTrue(ErrorConstructor(message).message === message, `message property is defined when ${ name } called with one argument, not cause`);
+                assert.isFalse("cause" in ErrorConstructor(message), `message property is defined when ${ name } called with one argument, not cause`);
+            }
+        },
+        {
+            name: `${ name }(${ message }, { cause: o })'s descriptor`,
+            body: function () {
+                const e = ErrorConstructor(message, { cause: o });
+                const desc = Object.getOwnPropertyDescriptor(e, "cause");
+                assert.areEqual(desc.configurable, true, "e.cause should be configurable");
+                assert.areEqual(desc.writable, true, "e.cause should be writable");
+                assert.areEqual(desc.enumerable, false, "e.cause should not be enumerable");
+            }
+        },
+        {
+            name: `o === ${ name }(${ message }, { cause: o }).cause`,
+            body: function () {
+                const e = Error();
+                assert.areEqual(ErrorConstructor("", { cause: o }).cause, o, `Cause property value should be kept as-is`);
+                assert.areEqual(ErrorConstructor("", { cause: 0 }).cause, 0, `Cause property value should be kept as-is`);
+                assert.areEqual(ErrorConstructor("", { cause: e }).cause, e, `Cause property value should be kept as-is`);
+                assert.areEqual(ErrorConstructor("", { cause: "A cause" }).cause, "A cause", `Cause property value should be kept as-is`);
+            }
+        },
+        {
+            name: "Proxy options parameter",
+            body: function () {
+                const options = new Proxy({ cause: o }, {
+                    has(target, p) {
+                        hasCounter++;
+                        return p in target;
+                    },
+                    get(target, p) {
+                        getCounter++;
+                        return target[p];
+                    }
+                });
+                var hasCounter = 0, getCounter = 0;
+                const e = ErrorConstructor("test", options);
+                assert.areEqual(hasCounter, 1, `hasCounter should be 1`);
+                assert.areEqual(getCounter, 1, `getCounter should be 1`);
+                assert.areEqual(e.cause, o, `Cause property value should be kept as-is`);
+                assert.areEqual(hasCounter, 1, `hasCounter should be 1`);
+                assert.areEqual(getCounter, 1, `getCounter should be 1`);
+            }
+        }
+    ]
+}
 const tests = [
-    {
-        name: "Error.prototype.cause",
-        body: function () {
-            assert.isFalse("cause" in Error.prototype, "Cause property must not exist in *Error.prototype");
-        }
-    },
-    {
-        name: "Error().cause === undefined",
-        body: function () {
-            assert.isFalse("cause" in SyntaxError(), "Error().cause should not be defined if specified by cause property of options parameter in *Error constructor");
-        }
-    },
-    {
-        name: `Error("", { cause: o })'s descriptor`,
-        body: function () {
-            const e = Error("", { cause: o });
-            const desc = Object.getOwnPropertyDescriptor(e, "cause");
-            assert.areEqual(desc.configurable, true, "e.cause should be configurable");
-            assert.areEqual(desc.writable, true, "e.cause should be writable");
-            assert.areEqual(desc.enumerable, false, "e.cause should not be enumerable");
-        }
-    },
-    {
-        name: "o === Error('', { cause: o }).cause",
-        body: function () {
-            assert.areEqual(RangeError("", { cause: o }).cause, o, `Cause property value should be kept as-is`);
-        }
-    },
-    {
-        name: "Proxy options parameter",
-        body: function () {
-            const options = new Proxy({ cause: o }, {
-                has(target, p) {
-                    hasCounter++;
-                    return p in target;
-                },
-                get(target, p) {
-                    getCounter++;
-                    return target[p];
-                }
-            });
-            var hasCounter = 0, getCounter = 0;
-            const e = ReferenceError("test", options);
-            assert.areEqual(hasCounter, 1, `hasCounter should be 1`);
-            assert.areEqual(getCounter, 1, `getCounter should be 1`);
-            assert.areEqual(e.cause, o, `Cause property value should be kept as-is`);
-            assert.areEqual(hasCounter, 1, `hasCounter should be 1`);
-            assert.areEqual(getCounter, 1, `getCounter should be 1`);
-        }
-    }
+    ...makeTestsFor(Error),
+    ...makeTestsFor(TypeError),
+    ...makeTestsFor(ReferenceError),
+    ...makeTestsFor(SyntaxError),
+    ...makeTestsFor(RangeError),
+    ...makeTestsFor(EvalError),
+    // TODO: Uncomment when #6301 is landed
+    // ...makeTestsFor(AggregateError)
 ];
 
 testRunner.runTests(tests, { verbose: WScript.Arguments[0] != "summary" });
