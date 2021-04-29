dc3794dbb83f4e4ad43514a7203fa80f41b5c7ff
MadProbe <49519179+MadProbe@users.noreply.github.com>
2021-04-30 00:31:42 +0300

    Add missing header in monotonicNow.js

--- test/Miscellaneous/monotonicNow.js
+++ test/Miscellaneous/monotonicNow.js
@@ -1,3 +1,9 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft Corporation and contributors. All rights reserved.
+// Copyright (c) 2021 ChakraCore Project Contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
 const now = WScript.monotonicNow;
 
 // Using <= instead of < because output number precision might be lost
