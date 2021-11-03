ac3739e25d3692a2b591a0e60b459701f0c12572
Petr Penzin <penzin.dev@gmail.com>
2021-11-03 00:17:25 -0700

    Bump version to 1.13
    For #6753

--- lib/Common/ChakraCoreVersion.h
+++ lib/Common/ChakraCoreVersion.h
@@ -1,5 +1,6 @@
 //-------------------------------------------------------------------------------------------------------
 // Copyright (C) Microsoft. All rights reserved.
+// Copyright (c) 2021 ChakraCore Project Contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 #pragma once
@@ -16,7 +17,7 @@
 
 // ChakraCore version number definitions (used in ChakraCore binary metadata)
 #define CHAKRA_CORE_MAJOR_VERSION 1
-#define CHAKRA_CORE_MINOR_VERSION 12
+#define CHAKRA_CORE_MINOR_VERSION 13
 #define CHAKRA_CORE_PATCH_VERSION 0
 #define CHAKRA_CORE_VERSION_RELEASE_QFE 0 // Redundant with PATCH_VERSION. Keep this value set to 0.
 
