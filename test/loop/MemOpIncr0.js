12184ad1499e3d6c0212242db71146da2a7532d5
rhuanjl <rhuan.l@gmail.com>
2022-01-22 12:25:46 +0000

    Fix incorrect failFast for Incr0

---
+++ test/loop/MemOpIncr0.js
@@ -0,0 +1,11 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft. All rights reserved.
+// Copyright (c) 2021 ChakraCore Project Contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
+for (let v3 = -65537; v3 < 3; v3 = v3 + 0) {
+    const v4 = v3++;
+}
+
+print("pass");
