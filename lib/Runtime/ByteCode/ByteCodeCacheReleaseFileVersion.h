a8809424bc42b78c057c09ab714c926186920c72
rhuanjl <rhuan.l@gmail.com>
2022-10-06 08:37:12 +0100

    Bytecode Regen

--- lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
+++ lib/Runtime/ByteCode/ByteCodeCacheReleaseFileVersion.h
@@ -1,12 +1,12 @@
 //-------------------------------------------------------------------------------------------------------
 // Copyright (C) Microsoft. All rights reserved.
-// Copyright (c) 2021 ChakraCore Project Contributors. All rights reserved.
+// Copyright (c) ChakraCore Project Contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 // NOTE: If there is a merge conflict the correct fix is to make a new GUID.
 // This file was generated with tools/regenByteCode.py
 
-// {0758006c-1750-4081-b107-c08101300fac}
+// {caa01c1a-6c67-45c2-8c3c-382b84ca34c4}
 const GUID byteCodeCacheReleaseFileVersion =
-{ 0x0758006c, 0x1750, 0x4081, {0xb1, 0x07, 0xc0, 0x81, 0x01, 0x30, 0x0f, 0xac } };
+{ 0xcaa01c1a, 0x6c67, 0x45c2, {0x8c, 0x3c, 0x38, 0x2b, 0x84, 0xca, 0x34, 0xc4 } };
 
