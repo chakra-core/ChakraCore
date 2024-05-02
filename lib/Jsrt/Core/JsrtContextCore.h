b211bb53d8a6e09fefbadac48ab8daf5fa46be1e
rhuanjl <rhuan.l@gmail.com>
2024-05-02 20:38:56 +0100

    Check for missing fetchImportedModuleFromScriptCallback

--- lib/Jsrt/Core/JsrtContextCore.h
+++ lib/Jsrt/Core/JsrtContextCore.h
@@ -1,5 +1,6 @@
 //-------------------------------------------------------------------------------------------------------
 // Copyright (C) Microsoft. All rights reserved.
+// Copyright (c) ChakraCore Project Contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 #pragma once
@@ -266,8 +267,6 @@ public:
 #endif
 
 private:
-    template<typename Fn>
-    HRESULT FetchImportedModuleHelper(Fn fetch, LPCOLESTR specifier, Js::ModuleRecordBase** dependentModuleRecord);
     FetchImportedModuleCallBack fetchImportedModuleCallback;
     FetchImportedModuleFromScriptCallBack fetchImportedModuleFromScriptCallback;
     NotifyModuleReadyCallback notifyModuleReadyCallback;
