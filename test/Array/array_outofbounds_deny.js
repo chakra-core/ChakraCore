8a67147f1c1a4a43085526f0ee78faddfeff2839
bhmohanr-techie <bhmohanr@gmail.com>
2024-07-08 00:02:13 -0700

    This change is in continuation to my earlier commit for adding unit tests... Added a change here to summarize the unit test results, so that the unit test framewokr can capture and report the test results.

--- test/Array/array_outofbounds_deny.js
+++ test/Array/array_outofbounds_deny.js
@@ -4,39 +4,84 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 
-WScript.Echo("*** Running test #1 (0): Deny out-of-bounds write");
+var helpers = function helpers() {
+    //private
+    var undefinedAsString = "undefined";
+
+    var isInBrowser = function isInBrowser() {
+        return typeof (document) !== undefinedAsString;
+    };
+
+    return {
+        // public
+        getDummyObject: function () {
+            //return isInBrowser() ? document : {};
+            return {};
+        },
+
+        writeln: function writeln() {
+            var line = "", i;
+            for (i = 0; i < arguments.length; i += 1) {
+                line = line.concat(arguments[i])
+            }
+            if (!isInBrowser()) {
+                WScript.Echo(line);
+            } else {
+                document.writeln(line);
+                document.writeln("<br/>");
+            }
+        },
+
+        printObject: function printObject(o) {
+            var name;
+            for (name in o) {
+                this.writeln(name, o.hasOwnProperty(name) ? "" : " (inherited)", ": ", o[name]);
+            }
+        }
+    }
+}(); // helpers module.
+
+var executedTestCount = 2;
+var passedTestCount = 0;
+
+helpers.writeln("*** Running test #1 (0): Deny out-of-bounds write");
 try {
     let arr = new Array(2);
-    WScript.Echo("arr.length is " + arr.length);
+    helpers.writeln("arr.length is " + arr.length);
 
     arr[0] = 0;
-    WScript.Echo("arr[0] is " + arr[0]);
+    helpers.writeln("arr[0] is " + arr[0]);
 
     arr[1] = 1;
-    WScript.Echo("arr[1] is " + arr[1]);
+    helpers.writeln("arr[1] is " + arr[1]);
 
     arr[100] = 100;
-    WScript.Echo("FAILED");
+    helpers.writeln("FAILED");
 } catch (e) {
-    WScript.Echo(e.name + ": " + e.message);
-    WScript.Echo("PASSED");
+    helpers.writeln(e.name + ": " + e.message);
+    helpers.writeln("PASSED");
+    ++passedTestCount;
 }
 
 
-WScript.Echo("*** Running test #2 (1): Deny out-of-bounds read");
+helpers.writeln("*** Running test #2 (1): Deny out-of-bounds read");
 try {
     let arr = new Array(2);
-    WScript.Echo("arr.length is " + arr.length);
+    helpers.writeln("arr.length is " + arr.length);
 
     arr[0] = 0;
-    WScript.Echo("arr[0] is " + arr[0]);
+    helpers.writeln("arr[0] is " + arr[0]);
 
     arr[1] = 1;
-    WScript.Echo("arr[1] is " + arr[1]);
+    helpers.writeln("arr[1] is " + arr[1]);
 
-    WScript.Echo("arr[100] is " + arr[100]);
-    WScript.Echo("FAILED");
+    helpers.writeln("arr[100] is " + arr[100]);
+    helpers.writeln("FAILED");
 } catch (e) {
-    WScript.Echo(e.name + ": " + e.message);
-    WScript.Echo("PASSED");
+    helpers.writeln(e.name + ": " + e.message);
+    helpers.writeln("PASSED");
+    ++passedTestCount;
 }
+
+helpers.writeln("Summary of tests: total executed: ", executedTestCount,
+        "; passed: ", passedTestCount, "; failed: ", executedTestCount - passedTestCount);
